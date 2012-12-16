dbd2251fc40be4f5d06689bbd1e385666969d218
Ignacio Cabrera Bywaters <cabrerabywaters@gmail.com>
2012-12-16 15:27:43 -0300

    Se crea la clase personas de la capa de datos
    Permitir  CRUD de personas

---
+++ Capa_Datos/Personas.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Personas
+ *
+ * @author Nacho
+ */
+class Personas {
+    //put your code here
+}
+
+?>
