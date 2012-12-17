06c9a27d0f13f55c1b8c7d37104c4983f8a6100f
Ignacio Cabrera Bywaters <cabrerabywaters@gmail.com>
2012-12-17 01:28:54 -0300

    mensaje de error para javascript

--- CRUD_INTERNO/registro_region.php
+++ CRUD_INTERNO/registro_region.php
@@ -5,9 +5,15 @@ require_once '../Capa_Datos/Region.php';
 $nombre= $_POST['nombre_region'];
 $numero= $_POST['numero_region'];
 
+if($nombre!="" and $numero!="")
+{
+
  $region= new Region($nombre,$numero);
 $region->Agregar_Regiones();
-
+}
+ else {
+ echo "<h5>* Faltan campos por completar</h5>"  ;
+}
  
 
   
