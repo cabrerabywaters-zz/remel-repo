2fac3303c585fb672fbdecba1f83741fce97ff17
César González Barreto <cgonzb@gmail.com>
2012-12-18 17:41:26 -0300

    Agregada la función de borrado de especialidad

--- Capa_Datos/especialidadesMedicas.php
+++ Capa_Datos/especialidadesMedicas.php
@@ -12,11 +12,13 @@
 class EspecialidadMedica {
     
     private $nombreEspecialidad;
+    private $idEspecialidad;
 
     
-    public function EspecialidadMedica($nombreEspecialidad){
+    public function EspecialidadMedica($nombreEspecialidad,$idEspecialidad){
         
         $this->nombreEspecialidad=$nombreEspecialidad; // defino el nombre de la especialidad
+        $this->idEspecialidad=$idEspecialidad; // defino el id de la especialidad
           }
       
           
@@ -37,6 +39,36 @@ class EspecialidadMedica {
       {
           
           echo "Especialidad $this->nombreEspecialidad Agregada con exito";
+         // mensaje de éxito
+      }
+      else
+      {
+          die('Error: ' . mysql_error());
+         // mensaje de error
+          
+      }
+      
+      $con->desconectar();
+        
+      
+    }
+    public function BorrarEspecialidades(){ //función que borra especialides en la bbdd retorna un mensaje de si se borró o no la especialidad
+        
+        $con = new ConexionDB(); // creo una nueva conexion a la db
+     
+       $con->conectar(); // me conecto a la db
+       
+      $idEspecialidad = mysql_real_escape_string($this->idEspecialidad); // transformo a un string real el nombre especialidad 
+      // seguridad
+     
+      
+      
+      $query= mysql_query("DELETE FROM Especialidades WHERE idEspecialidad ='$idEspecialidad'");
+      // query de borrado
+      if($query)
+      {
+          
+          echo "Especialidad $this->nombreEspecialidad borrada con exito";
          // mensaje de éxito
       }
       else
