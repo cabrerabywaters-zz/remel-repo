5c09bb0daa29f6cabb03630a925760520d152f22
mrbungie <geroa220@gmail.com>
2012-12-18 13:36:21 -0300

    Mas cambios a principio activo

--- Capa_Datos/Principio_Activo.php
+++ Capa_Datos/Principio_Activo.php
@@ -4,16 +4,25 @@ require_once 'Conexion.php';
 
 /**
 *Clase que representa y realiza todos los metodos de insercion, modificacion, seleccion y eliminacion en la tabla Principio Activo
-*@param $_nombrePrincipioActivo
+*@property string $_nombrePrincipioActivo
 **/
 
 class PrincipioActivo {
 
 	private $_nombrePrincipioActivo;
 	
+	/** Constructor para nombre a crear 
+	*@param string $nombrePrincipioActivo
+	**/
 	public function PrincipioActivo($nombrePrincipioActivo){
 		$this->_nombrePrincipioActivo = $nombrePrincipioActivo;
 	}	
+	
+	/** Constructor cuando no hay 
+	**/
+	public function PrincipioActivo(){
+		//Hacer nada
+	}
 	
 	public function AgregarPrincipioActivo(){
 		$nombrePrincipioActivo = mysql_real_escape_string($this->_nombrePrincipioActivo);
@@ -26,5 +35,8 @@ class PrincipioActivo {
 	else{
 	die('Error: ' . mysql_error());
 }
+	}
+
+	public function BuscarPorNombre(){
 	}
 }
