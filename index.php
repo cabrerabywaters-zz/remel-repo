902b33347a2d74405888f636c548227a6993edb3
mrbungie <geroa220@gmail.com>
2012-12-18 11:14:24 -0300

    Indentacion y comentarios añadidos a index.php <prueba de git en linux>

--- index.php
+++ index.php
@@ -1,92 +1,85 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Bootstrap 101 Template</title>
+	<head>
+        	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        	<title>Bootstrap 101 Template</title>
         
         
-        <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
-    <style type="text/css">
-      body {
-        padding-top: 40px;
-        padding-bottom: 40px;
-        background-color: #f5f5f5;
-      }
+        	<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+    		<style type="text/css">
+     			body {
+        			padding-top: 40px;
+        			padding-bottom: 40px;
+        			background-color: #f5f5f5;
+      			}
 
-      .form-signin {
-        max-width: 300px;
-        padding: 19px 29px 29px;
-        margin: 0 auto 20px;
-        background-color: #fff;
-        border: 1px solid #e5e5e5;
-        -webkit-border-radius: 5px;
-           -moz-border-radius: 5px;
-                border-radius: 5px;
-        -webkit-box-shadow: 0 1px 2px rgba(0,0,0,.05);
-           -moz-box-shadow: 0 1px 2px rgba(0,0,0,.05);
-                box-shadow: 0 1px 2px rgba(0,0,0,.05);
-      }
-      .form-signin .form-signin-heading,
-      .form-signin .checkbox {
-        margin-bottom: 10px;
-      }
-      .form-signin input[type="text"],
-      .form-signin input[type="password"] {
-        font-size: 16px;
-        height: auto;
-        margin-bottom: 15px;
-        padding: 7px 9px;
-      }
+      			.form-signin {
+        			max-width: 300px;
+        			padding: 19px 29px 29px;
+        			margin: 0 auto 20px;
+        			background-color: #fff;
+        			border: 1px solid #e5e5e5;
+        			-webkit-border-radius: 5px;
+           			-moz-border-radius: 5px;
+               			border-radius: 5px;
+        			-webkit-box-shadow: 0 1px 2px rgba(0,0,0,.05);
+           			-moz-box-shadow: 0 1px 2px rgba(0,0,0,.05);
+                		box-shadow: 0 1px 2px rgba(0,0,0,.05);
+      			}
+      			.form-signin .form-signin-heading,
+      			.form-signin .checkbox {
+        			margin-bottom: 10px;
+      			}
+      			.form-signin input[type="text"],
+      			.form-signin input[type="password"] {
+        			font-size: 16px;
+        			height: auto;
+        			margin-bottom: 15px;
+        			padding: 7px 9px;
+      			}
       
 
-    </style>
-       
-    </head>
+    		</style>   
+	</head>
+	
+	<!-- Library loading  -->
+    	<script src="http://code.jquery.com/jquery-latest.js"></script>
+     	<script src="js/bootstrap.min.js"></script>
+
+    	<body>
+
+
+    		<div class="container">
+
+      			<form class="form-signin">
+        			<h2 class="form-signin-heading">Bienvenido a                  Remel</h2>
+        			<input type="text" class="input-block-level" placeholder="Email address">
+        			<input type="password" class="input-block-level" placeholder="Password">
+        			<label class="checkbox">
+          				<input type="checkbox" value="remember-me"> Remember me
+        			</label>
+        			<button class="btn btn-large btn-primary" type="submit">Ingresar</button>
+      			</form>
+
+    		</div> <!-- /container -->
+
+    	<!-- Le javascript
+    	================================================== -->
+    	<!-- Placed at the end of the document so the pages load faster -->
+    	<script src="../assets/js/jquery.js"></script>
+    	<script src="../assets/js/bootstrap-transition.js"></script>
+    	<script src="../assets/js/bootstrap-alert.js"></script>
+    	<script src="../assets/js/bootstrap-modal.js"></script>
+    	<script src="../assets/js/bootstrap-dropdown.js"></script>
+    	<script src="../assets/js/bootstrap-scrollspy.js"></script>
+    	<script src="../assets/js/bootstrap-tab.js"></script>
+    	<script src="../assets/js/bootstrap-tooltip.js"></script>
+    	<script src="../assets/js/bootstrap-popover.js"></script>
+    	<script src="../assets/js/bootstrap-button.js"></script>
+    	<script src="../assets/js/bootstrap-collapse.js"></script>
+    	<script src="../assets/js/bootstrap-carousel.js"></script>
+    	<script src="../assets/js/bootstrap-typeahead.js"></script>
     
-     <script src="http://code.jquery.com/jquery-latest.js"></script>
-     <script src="js/bootstrap.min.js"></script>
-    <body>
-
-
-    <div class="container">
-
-      <form class="form-signin">
-        <h2 class="form-signin-heading">Bienvenido a                  Remel</h2>
-        <input type="text" class="input-block-level" placeholder="Email address">
-        <input type="password" class="input-block-level" placeholder="Password">
-        <label class="checkbox">
-          <input type="checkbox" value="remember-me"> Remember me
-        </label>
-        <button class="btn btn-large btn-primary" type="submit">Ingresar</button>
-      </form>
-
-    </div> <!-- /container -->
-
-    <!-- Le javascript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="../assets/js/jquery.js"></script>
-    <script src="../assets/js/bootstrap-transition.js"></script>
-    <script src="../assets/js/bootstrap-alert.js"></script>
-    <script src="../assets/js/bootstrap-modal.js"></script>
-    <script src="../assets/js/bootstrap-dropdown.js"></script>
-    <script src="../assets/js/bootstrap-scrollspy.js"></script>
-    <script src="../assets/js/bootstrap-tab.js"></script>
-    <script src="../assets/js/bootstrap-tooltip.js"></script>
-    <script src="../assets/js/bootstrap-popover.js"></script>
-    <script src="../assets/js/bootstrap-button.js"></script>
-    <script src="../assets/js/bootstrap-collapse.js"></script>
-    <script src="../assets/js/bootstrap-carousel.js"></script>
-    <script src="../assets/js/bootstrap-typeahead.js"></script>
-
-
-    
-  </body>
- 
-
-   
- 
-   
-
+	</body>
 
 </html>
