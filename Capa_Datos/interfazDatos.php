1df41ab33dd7e384b09951c16f6e20e2cd5487fd
José-Francisco González <pratiko2010@gmail.com>
2012-12-19 16:17:15 -0400

    interefazDatos sin CallQuery (con include CallQuery.php)

--- Capa_Datos/interfazDatos.php
+++ Capa_Datos/interfazDatos.php
@@ -7,6 +7,7 @@
 * @autor: German Oviedo 
 ***/
 
+include "CallQuery.php";
 
 /**
 * Funcion que crea el string para la query de Seleccionar
@@ -67,22 +68,4 @@ function QueryStringActualizar($where, $datos, $nombreTabla){
 	return $updateString;	
 }
 
-function CallQuery($queryString){
-		include('dbconfig.php');
-		$mysqlCon = new mysqli($servidor,$nombre_usuario,$contrasena,$base_de_datos);
-
-		if($mysqlCon->errno) {
-			printf("Conexion fallida: %s\n", $mysqli->connect_error);
-			exit();
-		}
-		
-		if($result = $mysqlCon->query($queryString)){
-			$mysqlCon->close();
-			return $result;
-		}     
-		else{
-			$mysqlCon->close();
-			return $false;
-		}  
-}
-
+?>
