31f4e008068dac1e2a3f17c99f96aea5dedcfca2
José-Francisco González <pratiko2010@gmail.com>
2012-12-19 10:04:37 -0400

    Actualizacion de Codigo en tabla
    tablas: tipos_recetas

--- Capa_Datos/tipoRecetas.php
+++ Capa_Datos/tipoRecetas.php
@@ -1,41 +1,59 @@
 <?php
 
-class Tipos_Recetas {
+require_once 'interfazDatos.php';
 
+/**
+*Clase que representa y realiza todos los metodos de insercion, modificacion, seleccion y eliminacion en la tabla Principio Activo
+*@property string $_nombrePrincipioActivo
+**/
+
+class TiposRecetas {
+
+    const nombreTabla = "Tipos_Recetas";
+    const nombreIdTabla = "idTipos_Recetas";
+    
     private $_datos;
     private $_id;
 
     //Instanciacion 
 
-    public function Tipos_Recetas($id) {
+    public function principioActivo($id) {
 
         $this->_id = $id;
     }
-
-    public static function Agregar($datos) {
-        $queryString = QueryStringAgregar($datos, "Tipos_Recetas");
-        $query = CallQuery($queryString);
+    
+    
+	public static function Agregar($datos){
+    	$queryString = QueryStringAgregar($datos,nombreTabla);
+    	$query = CallQuery($queryString);
     }
 
-    public function BorrarPorId() {
-        $queryString = QueryStringBorrarPorId("Tipos_Recetas", "idTipos_Recetas", $_id);
-        $query = CallQuery($queryString);
+    /**
+    * Metodo para agregar funciones a la tabla
+    **/
+    public function BorrarPorId(){
+	$queryString = QueryStringBorrarPorId(nombreTabla,nombreIdTabla,$_id);
+	$query = CallQuery($queryString);
+    }        
+    
+    /**
+    * Metodo para agregar funciones a la tabla
+    * @param array $datos Vienen del controlador
+    **/
+    public function Actualizar($datos){
+	$where = "WHERE ".nombreIdTabla." = '$id'";
+	$queryString = QueryStringActualizar($where,$datos,nombreTabla);
+	$query = CallQuery($queryString);
     }
-
-    public function Actualizar($datos) {
-        // Frase WHERE
-        $where = "WHERE ...";
-        $queryString = QueryStringActualizar($where, $datos, "Tipos_Recetas");
-        $query = CallQuery($queryString);
+    
+    /**
+    * Metodo para agregar funciones a la tabla
+    * @param array $atributosASeleccionar Vienen del controlador
+    * @param array $where Frase Where que es indicada por el controlador
+    **/
+    public function Seleccionar($atributosASeleccionar,$where){
+	$queryString = QueryStringSeleccionar($where,$atributosASeleccionar,nombreTabla);
+	$query = CallQuery($queryString);
+	//TODO: Falta el proceso de llenado de populado del objeto
     }
-
-    public function Seleccionar($atributosASeleccionar) {
-        // Frase WHERE
-        $where = "WHERE ...";
-        $queryString = QueryStringSeleccionar($where, $atributosASeleccionar, "Tipos_Recetas");
-        $query = CallQuery($queryString);
-        //TODO: Falta el proceso de llenado de populado del objeto
-    }
-
 }
-?>
