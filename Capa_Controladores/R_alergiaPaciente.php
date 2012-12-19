6b898fc3fdbfd3453af03e2969f321310323cef4
José-Francisco González <pratiko2010@gmail.com>
2012-12-19 13:47:59 -0400

    ADSAD

---
+++ Capa_Controladores/R_alergiaPaciente.php
@@ -0,0 +1,24 @@
+<html>
+    <form action="R_alergiaPaciente.php" method="post">
+    <input type="text" name="id_Alergia">
+    <input type="text" name="id_Paciente">
+    <input type="submit">
+    </form>
+</html>
+
+<?php
+
+require_once '../Capa_Datos/R_alergiaPaciente.php';
+
+function Creacion() {
+    $datosCreacion = array(
+        array('Alergia_idAlergia', $_POST['id_Alergia']),
+        array('Paciente_idPaciente', $_POST['id_Paciente'])
+    );
+    if ($_POST['id_Alergia'] != '') {
+        R_AlergiaPaciente::CrearRelacion($datosCreacion);
+    }
+    
+}
+Creacion();
+?>
