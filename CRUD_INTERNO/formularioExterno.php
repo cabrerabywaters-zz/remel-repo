83755acb84a8ba4360b259101905ceddc51d4bde
Leonardo Hidalgo <leonardo@gmail.com>
2012-12-20 12:14:18 -0300

    creacion del formulario

--- CRUD_INTERNO/formularioExterno.php
+++ CRUD_INTERNO/formularioExterno.php
@@ -5,19 +5,5 @@ $arreglo=array(array("nombre"=>1,"id"=>2,"fecha"=>3,"dias"=>4),array("nombre"=>1
 function creacionFormularios($arreglo)
 {
 	
-	foreach ($arreglo as $datos)
-	{
-	foreach ($datos as $dato=>$valor)
-	{?>
-		
-	<?php
-    }
-	}
-	
 }
-foreach ()
-{
-	
-}
-creacionFormularios($arreglo);
 ?>
