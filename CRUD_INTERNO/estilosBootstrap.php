01bdc31d71af814bd113418f5a2ee194916335b7
César González Barreto <cgonzb@gmail.com>
2012-12-20 14:28:15 -0300

    editadas rutas de js

--- CRUD_INTERNO/estilosBootstrap.php
+++ CRUD_INTERNO/estilosBootstrap.php
@@ -6,11 +6,8 @@
  */
 ?>
     <!-- Le styles -->
-    <link href="css/bootstrap.css" rel="stylesheet"> <!-- Estilo css de bootstrap -->
-    <link href="css/navegacion.ss" rel="stylesheet"> <!-- Estilo de la barra de navegacion -->
-    <link href="css/bootstrap-responsive.css" rel="stylesheet">
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
-    <link rel="shortcut icon" href="../assets/ico/favicon.png">
+    <link href="../css/bootstrap.css" rel="stylesheet"> <!-- Estilo css de bootstrap -->
+    <link href="../css/navegacion.ss" rel="stylesheet"> <!-- Estilo de la barra de navegacion -->
+    <link href="../css/bootstrap-responsive.css" rel="stylesheet">
+    
+    
