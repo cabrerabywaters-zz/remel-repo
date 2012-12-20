9c9343605a63d691e9aadf0acc802772ab285a9d
César González Barreto <cgonzb@gmail.com>
2012-12-20 14:27:44 -0300

    movido arreglo de ejemplo a crudContenido.php

--- CRUD_INTERNO/crudContenido.php
+++ CRUD_INTERNO/crudContenido.php
@@ -14,8 +14,22 @@
     <div class="span10" id="contenido">
 
 <?php // aqui se incluye el formulario o la vista correspondiente
-include "vistaEjemplo.php";
+// este es un arreglo de ejemplo
+$arreglo=array( //arreglo de pruebas
+    array("nombre"=>"asdasdas","id"=>2,"fecha"=>"hoflgldfls","dias"=>"juanito"),
+    array("nombre"=>"juan","id"=>"pedros","fecha"=>3,"dias"=>4),
+    array("nombre"=>"juan","id"=>"pedros","fecha"=>3,"dias"=>4),
+    array("nombre"=>"juan","id"=>"pedros","fecha"=>3,"dias"=>4),
+    array("nombre"=>"juan","id"=>"pedros","fecha"=>3,"dias"=>4),
+    array("nombre"=>"juan","id"=>"pedros","fecha"=>3,"dias"=>4));
+
+
+require "vistaEjemplo.php"; //funcion que crea la vista de un arreglo
+require 'formularioExterno.php'; // funcion que crea el formulario de un arreglo
+
 visualizacionTabla($arreglo);
+creacionFormularios($arreglo);
 ?>
+        <script ref="text/javascript" src="js/validarFormExterno.js"></script>  
     </div>
 </div>
