5c34669471e9bb7b62f28ef16f317f3563e62d76
César González Barreto <cgonzb@gmail.com>
2012-12-20 11:22:46 -0300

    Agregadas delimitaciones de <div>

--- CRUD_INTERNO/index.php
+++ CRUD_INTERNO/index.php
@@ -3,8 +3,8 @@ include 'crudHeader.php'; // header del crud con la inclusion de css y jQuery
 include 'crudNavBar.php';
 ?>
     
-<div class="container-fluid">
-  <div class="row-fluid">
+<div class="container-fluid"><!-- Contenedor MAIN-->
+  <div class="row-fluid"><!-- Contenedor FILA-->
     
 <?php
 include 'crudSideBar.php';
@@ -14,7 +14,8 @@ include 'crudSideBar.php';
     <div class="span10"><!--Body content-->
         <h1>aqui contenido</h1>
     </div><!-- fin body content -->
-</div>
+   </div><!-- fin contenedor FILA -->
+</div><!-- fin contenedor MAIN-->
 <?php
 include'crudFooter.php';
 ?>
