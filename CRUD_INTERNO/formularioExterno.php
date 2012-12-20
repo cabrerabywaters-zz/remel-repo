921fed8a2225ff5494e0d234fd47f03f349e2796
Leonardo Hidalgo <leonardo@gmail.com>
2012-12-20 12:59:11 -0300

    creacion del formulario del crud

--- CRUD_INTERNO/formularioExterno.php
+++ CRUD_INTERNO/formularioExterno.php
@@ -1,9 +1,30 @@
 <?php
 
-$arreglo=array(array("nombre"=>1,"id"=>2,"fecha"=>3,"dias"=>4),array("nombre"=>1,"id"=>2,"fecha"=>3,"dias"=>4),array("nombre"=>1,"id"=>2,"fecha"=>3,"dias"=>4),array("nombre"=>1,"id"=>2,"fecha"=>3,"dias"=>4),array("nombre"=>1,"id"=>2,"fecha"=>3,"dias"=>4),array("nombre"=>1,"id"=>2,"fecha"=>3,"dias"=>4),array("nombre"=>1,"id"=>2,"fecha"=>3,"dias"=>4),array("nombre"=>1,"id"=>2,"fecha"=>3,"dias"=>4),array("nombre"=>1,"id"=>2,"fecha"=>3,"dias"=>4),array("nombre"=>1,"id"=>2,"fecha"=>3,"dias"=>4));
+$arreglo=array(array('nombre'=>1,'id'=>2,'fecha'=>3,'dias'=>4),array('nombre'=>1,'id'=>2,'fecha'=>3,'dias'=>4),array('nombre'=>1,'id'=>2,'fecha'=>3,'dias'=>4),array('nombre'=>1,'id'=>2,'fecha'=>3,'dias'=>4),array('nombre'=>1,'id'=>2,'fecha'=>3,'dias'=>4),array('nombre'=>1,'id'=>2,'fecha'=>3,'dias'=>4),array('nombre'=>1,'id'=>2,'fecha'=>3,'dias'=>4),array('nombre'=>1,'id'=>2,'fecha'=>3,'dias'=>4),array('nombre'=>1,'id'=>2,'fecha'=>3,'dias'=>4),array('nombre'=>1,'id'=>2,'fecha'=>3,'dias'=>4));
 
 function creacionFormularios($arreglo)
-{
-	
+{ 
+	$array=$arreglo[0];
+
+	echo "<form class='form-horizontal'>";
+  
+	foreach ($array as $dato => $valor)
+	{
+    
+	echo "<div class='control-group'>";
+    echo"<label class='control-label'>".ucfirst($dato)."</label>";
+    echo"<div class='controls'>";
+      echo"<input type='text'  placeholder='".ucfirst($dato)."'>";
+    echo"</div>";
+  echo"</div>";	
+	 }
+  echo"<div class='control-group'>";
+    echo"<div class='controls'>";
+      
+      echo"<button type='submit' class='btn'>Agregar</button>";
+    echo"</div>";
+  echo"</div>";
+echo"</form>";
+
 }
 ?>
