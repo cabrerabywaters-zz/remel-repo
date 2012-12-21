fb2822713009d21f71d66a807f158ba6a15ca64c
César González Barreto <cgonzb@gmail.com>
2012-12-21 14:43:19 -0300

    validador del formulario
    validador del formulario ahora crea la url para ser enviada por AJAX al
    controlador indicado en var=donde estoy

--- js/crudValidarFormExterno.js
+++ js/crudValidarFormExterno.js
@@ -5,17 +5,59 @@
  */  
 
 $(document).ready(function(){
-    $('#agregar-nuevo').click(function(){
-        // get all the inputs into an array.
+    $('#agregar').click(function(){
+            // get all the inputs into an array.
     var $inputs = $('#formulario :input');
 
-    // not sure if you wanted this, but I thought I'd add it.
-    // get an associative array of just the values.
-    var values = {}; // definicion del arreglo values
-    $inputs.each(function() { 
-        values[this.name] = $(this).val();// agarra cada input y lo guarda en el arreglo
-        });//end each
+    // Se crea el objeto XMLHttpRequest 
+    var hr = new XMLHttpRequest();
+    // Se crean las variables que serán enviadas
+    
+    // url de la página a la cual se enviarán las vairables
+    var url = "../Capa_Controladores/region.php";
+    
+        //creacion de la variable para enviar via post
+    var envio = "";
+    $inputs.each(function() { // se toma cada input y se desglosa
+        var indice = $(this).attr('name'); // indice del input
+        var valor = $(this).val(); // valor del elemento
+     
+        envio = envio+indice+"="+valor+'&'; // se arma la direccion de envío de AJAX
+        
+        if(this.value == ""){ // se verifica que el input no esté vacío
+        // se cambia las caracteristicas del div donde está el input
+        $('input[name="'+indice+'"]').parent().parent().attr('class','control-group error'); // se cambia la clase del div donde está a c
+        //error
+        $('input[name="'+indice+'"]').attr('id','inputError');
+        //se cambia el diseño del input a error
+        $('input[name="'+indice+'"]').attr('placeholder','Campo no ingresado');
+        }
+        else{ // se vuelve a la normalidad el campo
+            
+        $('input[name="'+indice+'"]').parent().parent().attr('class','control-group');
+        $('input[name="'+indice+'"]').attr('id',indice);
+        $('input[name="'+indice+'"]').attr('placeholder','Campo no ingresado');
+        }
+            
+        
+            
+            
+            
+       
+        
+    });//end each
+    envio = envio+'var=dondeEstoy'; // SE DEBE AGREGAR UNA VARIABLE QUE INDIQUE EN QUE SECCION ESTOY
+    //envio final por POST
+    hr.open("POST", url, true);
+    // Set content type header information for sending url encoded variables in the request
+    hr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    // Access the onreadystatechange event for the XMLHttpRequest object
+    
+    
+    
+    
 
-    });// end click
-});//end ready
+
+    });//end click
+    });//end ready
     
