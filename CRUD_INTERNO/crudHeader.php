21de710b6afd151a3175f46a570e72073647b1c5
César González Barreto <cgonzb@gmail.com>
2012-12-21 11:30:51 -0300

    modificado acordeon

--- CRUD_INTERNO/crudHeader.php
+++ CRUD_INTERNO/crudHeader.php
@@ -10,20 +10,21 @@
 
 
     <script src="http://code.jquery.com/jquery-latest.js"></script>
-    <script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/js/bootstrap.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+    <script src="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/js/bootstrap.min.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
     <link href="../css/bootstrap.css" rel="stylesheet"> <!-- Estilo css de bootstrap -->
-    <link href="../css/bootstrap-responsive.css" rel="stylesheet">
-    <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/css/bootstrap-combined.min.css" rel="stylesheet">
-    <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/css/bootstrap.no-icons.min.css" rel="stylesheet">
-    
+    <link href="../css/bootstrap-responsive.css" rel="stylesheet"><!-- estilo responsive de bootstrap -->
+    <link href="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/css/bootstrap-combined.min.css" rel="stylesheet">
+    <link href="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/css/bootstrap.no-icons.min.css" rel="stylesheet">
+    <script src="http://code.jquery.com/jquery-1.8.3.js"></script>
+     <script src="http://code.jquery.com/ui/1.9.2/jquery-ui.js"></script>
     <script type="text/javascript">                                         
-    $(document).ready(function() {
-      // para que los acordiones inicien colapsados
-    $('.accordion-body').collapse({
-     toggle: true
-      })
-        }); //end ready
+   $(document).ready(function() {
+        $( "#accordion" ).accordion({
+            collapsible: true
+        });
+    }); // end ready
+    
     </script>
     
 </head>
