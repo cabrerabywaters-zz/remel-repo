0e06a42aeb4f64678a7cf2d5856521e465aec419
César González Barreto <cgonzb@gmail.com>
2012-12-21 14:41:53 -0300

    modificada funcion que crea formulario de tabla externa
    ahora el boton se encuentra fuera de la función

--- CRUD_INTERNO/formularioExterno.php
+++ CRUD_INTERNO/formularioExterno.php
@@ -14,30 +14,21 @@ function creacionFormularios($arreglo)
 { // igualacion de el primer array a una variable para usarla mas adelante
 	$array= $arreglo[0];
 // se abre el bootstap para la creacion del formulario
-	echo "<form class='form-horizontal' id='formulario'>";
+	echo "
+            <form class='form-horizontal' id='formulario'>";
   // se recorre el arreglo
 	foreach ($array as $dato => $valor)
 	{
-    	echo "<div class='control-group'>";
-	// es el label que muestra datos
-    echo"<label class='control-label'>".ucfirst($dato)."</label>";
-	
-    echo"<div class='controls'>";
-	// es el input en donde se ingresan los datos
-      echo"<input type='text'  placeholder='".ucfirst($dato)."'>";
-	  
-    echo"</div>";
-  echo"</div>";	
+    	echo "
+            <div class='control-group' id='$dato'>
+            <label name='$dato' class='control-label'>".ucfirst($dato)."</label>
+                <div class='controls' id='$dato-control'>
+                <input type='text'  name ='$dato' placeholder='".ucfirst($dato)."' id='inputError'>
+                </div>
+              </div>";
 	 }
 	 // es la division que permite la cracion del boton
-  echo"<div class='control-group'>";
-  // es el tipo de boton que se asigna
-    echo"<div class='controls'>";
-      // es el boton en si con su respectivo formato
-      echo"<button type='submit' class='btn btn-inverse' id='agregar'>Agregar</button>";
-    echo"</div><!-- cierre div controls->";
-  echo"</div><!-- cierre div control-group";
-echo"</form><!--cierre div horizontal-group-->";
-
+  echo "
+      </form><!-- cierre div horizontal-group-->";
 }
 ?>
