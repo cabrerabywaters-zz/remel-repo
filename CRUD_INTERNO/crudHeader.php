4c26aa7f046cb7e649d89637f1e32b6177e8b0ea
Leonardo Hidalgo <leonardo@gmail.com>
2012-12-26 10:40:01 -0300

    cambien en la tabla

--- CRUD_INTERNO/crudHeader.php
+++ CRUD_INTERNO/crudHeader.php
@@ -26,8 +26,39 @@
         });
         $('#datepicker').datepicker();
     }); // end ready
-    
+    <!-- scripts de la datetable -->
     </script>
+    
+		<script type="text/javascript" language="javascript" src="../js/jquery.jeditable.js"></script>
+		<script type="text/javascript" language="javascript" src="../js/jquery.dataTables.js"></script>
+        <script type="text/javascript" charset="utf-8">
+			$(document).ready(function() {
+				/* Init DataTables */
+				var oTable = $('#example').dataTable();
+				
+				/* Apply the jEditable handlers to the table */
+				oTable.$('td').editable( '../examples_support/editable_ajax.php', {
+					"callback": function( sValue, y ) {
+						var aPos = oTable.fnGetPosition( this );
+						oTable.fnUpdate( sValue, aPos[0], aPos[1] );
+					},
+					"submitdata": function ( value, settings ) {
+						return {
+							"row_id": this.parentNode.getAttribute('id'),
+							"column": oTable.fnGetPosition( this )[2]
+						};
+					},
+					"height": "14px",
+					"width": "100%"
+				} );
+			} );
+		</script>
+    <!-- estilos para la tabla-->
+    	<style type="text/css" title="currentStyle">
+			@import "../css/demo_page.css";
+			@import "../css/demo_table.css";
+		</style>
+        
     
 </head>
 <body>
