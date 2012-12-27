86f879ddc4d225b2d19738a81f23c1049a83249a
César González Barreto <cgonzb@gmail.com>
2012-12-27 00:10:16 -0300

    agregadas variable global de tabla y accion
    listo envio de variables via ajax para la muestra dinamica de las tablas
    del crud, solo falta hacer el manejo correspondiente mediante el
    controlador

--- js/crudVisualizacion.js
+++ js/crudVisualizacion.js
@@ -6,5 +6,16 @@
 
 $(document).ready(function(){
 	
- 
+   $('#accordion a').click(function(evento){
+        evento.preventDefault();
+        tabla = $(this).attr('href'); // asigno la tabla al boton seleccionado
+        accion = 0; //asigno la accion predeterminada de mostrar los elementos de la tabla
+        $("#contenido").load("../Capa_Controladores/controladorPruebas.php", {Tabla: tabla, Accion: accion});
+              
+          
+        
+        
+   }); //end click
+
+
 });//end ready
