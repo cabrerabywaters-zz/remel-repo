3fc1b0f8047081839e64a6f7a64f316b3a7dd7e5
César González Barreto <cgonzb@gmail.com>
2012-12-28 13:48:42 -0300

    ahora el crud muestra correctamente la tabla
    y el formulario

--- js/crudVisualizacion.js
+++ js/crudVisualizacion.js
@@ -5,18 +5,24 @@
  */
 
 $(document).ready(function(){
-	
+	/**
+         * on click se muestra la tabla de la base de datos (via ajax)
+         */
    $('#accordion a').click(function(evento){
         evento.preventDefault();
         tabla = $(this).attr('href'); // asigno la tabla al boton seleccionado
         accion = 0; //asigno la accion predeterminada de mostrar los elementos de la tabla
-        $("#contenido").load("../Capa_Controladores/filtroJsControlador.php", {Tabla: tabla, Accion: accion}, function(){
-            
+        $("#contenido").load("../Capa_Controladores/filtroJsControlador.php", {Tabla: tabla, Accion: accion, form: 0}, function(){
+         
         });
-              
-          
-        
-        
+   }); //end click   
+   $('#accordion a').click(function(evento){
+        evento.preventDefault(); //prevengo que el link funcione
+        tabla = $(this).attr('href'); // asigno la tabla al boton seleccionado
+        accion = 0; //asigno la accion predeterminada de mostrar los elementos de la tabla
+        $('#myModalLabel').html('Agregar elemento a '+tabla);
+        $("#modal-body").load("../Capa_Controladores/filtroJsControlador.php", {Tabla: tabla, Accion: accion, form: 1});
+          // AQUI SE DEBE MANDAR LA INFORMACION VIA AJAX AL ARCHIVO CORRESPONDIENTE PARA QUE SEA PROCESADA
    }); //end click
 
 
