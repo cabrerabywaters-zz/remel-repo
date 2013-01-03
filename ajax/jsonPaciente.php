717ac9f4aea4dd6f5e6833cca605fa740c922bfa
mrbungie <geroa220@gmail.com>
2013-01-03 14:35:18 -0300

    asDASDAS

--- ajax/jsonPaciente.php
+++ ajax/jsonPaciente.php
@@ -8,9 +8,9 @@ include dirname(__FILE__)."/../Capa_Controladores/paciente.php";
 
 $rut = $_POST['RUN'];
 //echo $rut; 
- $rut2=str_replace(".","",$rut);//elimina puntos del rut
-    $rut3=str_replace("-","",$rut2);//elimina guiones del rut
-    $run=$rut3; //iguala la variable final a la variable inicial
+$rut2=str_replace(".","",$rut);//elimina puntos del rut
+$rut3=str_replace("-","",$rut2);//elimina guiones del rut
+$run=$rut3; //iguala la variable final a la variable inicial
 
 $array1 = PacienteSeleccionIdPorRUN($run);
 $array2 = PersonaSeleccionNombrePorRUN($run);
