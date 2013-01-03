50cdafb77eba0f72679283f0fb18ce0d0ecb24e9
maximiano silva <maxsilva@alumnos.uai.cl>
2013-01-03 16:24:04 -0300

    +controladores

---
+++ Capa_Controladores/funcionario.php
@@ -0,0 +1,90 @@
+<?php 
+
+include_once('../Capa_Datos/generadorStringQuery.php');
+
+class Funcionario {
+
+    static $nombreTabla = "Funcionarios";
+    static $nombreIdTabla = "idFuncionario";    
+    
+    /**
+     * Insertar
+     * 
+     * Inserta una nueva entrada
+     * 
+     */
+    public static function Insertar() {
+    	$datosCreacion = array(
+                                array('Telefono',$_POST['telefono_funcionario']),
+                                      );
+
+        $queryString = QueryStringAgregar($datosCreacion, self::$nombreTabla);
+        $query = CallQuery($queryString);
+    }
+
+    /**
+     * BorrarPorId
+     * 
+     * Borra una entrada segun su id, pasada por POST.
+     */
+    public static function BorrarPorId() {
+        $id = $_POST['id'];
+        $queryString = QueryStringBorrarPorId(self::$nombreTabla, self::$nombreIdTabla, $this->_id);
+        $query = CallQuery($queryString);
+    }
+    
+    /**
+     * Seleccionar
+     * 
+     * Esta funcion selecciona todas las entradas de una tabla
+     * con respecto a una condicion dada. Tambien es posible
+     * entregar un limite y un offset.
+     * 
+     * @param string $where
+     * @param int $limit
+     * @param int $offset
+     * @returns array $resultArray
+     */
+    public static function Seleccionar($where, $limit = 0, $offset = 0) {
+    	$atributosASeleccionar = array(
+                                       'Telefono'
+            );
+
+        $queryString = QueryStringSeleccionar($where, $atributosASeleccionar, self::$nombreTabla);
+
+	    if($limit != 0){
+	       $queryString = $queryString." LIMIT $limit";
+	    }
+	    if($offset != 0){
+		  $queryString = $queryString." OFFSET $offset ";
+	    }
+
+        $result = CallQuery($queryString);
+	    $resultArray = array();
+	    while($fila = $result->fetch_assoc()) {
+	       $resultArray[] = $fila;
+	    }
+	    return $resultArray;
+    }
+    
+    /**
+     * Actualizar
+     * 
+     * Esta funcion toma una id de una entrada existente
+     * y actualiza con datos nuevos, la id y los datos vienen
+     * por POST desde AJAX
+     */
+    public static function Actualizar() {
+    	$id = $_POST['id_condiciones'];
+    	$datosActualizacion = array(
+                                array('Telefono',$_POST['telefono_funcionario']),
+                                	);
+
+        $where = "WHERE " . self::$nombreIdTabla . " = '$id'";
+        $queryString = QueryStringActualizar($where, $datosActualizacion, self::$nombreTabla);
+        $query = CallQuery($queryString);
+    }
+
+}
+
+?>
---
+++ Capa_Controladores/prevision.php
@@ -0,0 +1,90 @@
+<?php 
+
+include_once('../Capa_Datos/generadorStringQuery.php');
+
+class Prevision {
+
+    static $nombreTabla = "Previsiones";
+    static $nombreIdTabla = "rut";    
+    
+    /**
+     * Insertar
+     * 
+     * Inserta una nueva entrada
+     * 
+     */
+    public static function Insertar() {
+    	$datosCreacion = array(
+                                array('Nombre',$_POST['nombre_especialidad']),
+                                      );
+
+        $queryString = QueryStringAgregar($datosCreacion, self::$nombreTabla);
+        $query = CallQuery($queryString);
+    }
+
+    /**
+     * BorrarPorId
+     * 
+     * Borra una entrada segun su id, pasada por POST.
+     */
+    public static function BorrarPorId() {
+        $id = $_POST['id'];
+        $queryString = QueryStringBorrarPorId(self::$nombreTabla, self::$nombreIdTabla, $this->_id);
+        $query = CallQuery($queryString);
+    }
+    
+    /**
+     * Seleccionar
+     * 
+     * Esta funcion selecciona todas las entradas de una tabla
+     * con respecto a una condicion dada. Tambien es posible
+     * entregar un limite y un offset.
+     * 
+     * @param string $where
+     * @param int $limit
+     * @param int $offset
+     * @returns array $resultArray
+     */
+    public static function Seleccionar($where, $limit = 0, $offset = 0) {
+    	$atributosASeleccionar = array(
+                                        'Nombre',
+      );
+
+        $queryString = QueryStringSeleccionar($where, $atributosASeleccionar, self::$nombreTabla);
+
+	    if($limit != 0){
+	       $queryString = $queryString." LIMIT $limit";
+	    }
+	    if($offset != 0){
+		  $queryString = $queryString." OFFSET $offset ";
+	    }
+
+        $result = CallQuery($queryString);
+	    $resultArray = array();
+	    while($fila = $result->fetch_assoc()) {
+	       $resultArray[] = $fila;
+	    }
+	    return $resultArray;
+    }
+    
+    /**
+     * Actualizar
+     * 
+     * Esta funcion toma una id de una entrada existente
+     * y actualiza con datos nuevos, la id y los datos vienen
+     * por POST desde AJAX
+     */
+    public static function Actualizar() {
+    	$id = $_POST['id_condiciones'];
+    	$datosActualizacion = array(
+                                array('Nombre',$_POST['nombre_plaza_institucion']),
+                );
+
+        $where = "WHERE " . self::$nombreIdTabla . " = '$id'";
+        $queryString = QueryStringActualizar($where, $datosActualizacion, self::$nombreTabla);
+        $query = CallQuery($queryString);
+    }
+
+}
+
+?>
