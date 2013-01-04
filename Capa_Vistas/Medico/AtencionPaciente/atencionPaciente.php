117d8d31cd93e0193333ae25895a14e85228f95c
César González Barreto <cgonzb@gmail.com>
2013-01-04 13:55:34 -0300

    externalizacion modulos atencion paciente

--- Capa_Vistas/Medico/AtencionPaciente/atencionPaciente.php
+++ Capa_Vistas/Medico/AtencionPaciente/atencionPaciente.php
@@ -14,67 +14,47 @@ include('../../medicoHeader.php'); // elementos visuales, navegacion y encabezad
 
 
   <div class="tab-content"><!-- contenido del panel 1-->
+    
     <div class="tab-pane active img-rounded" id="tabHistorial"><!-- tab Historial-->
       
-        <div class="accordion" id="accordion2">
-  <div class="accordion-group">
-    <div class="accordion-heading">
-      <a class="btn btn-large btn-block btn-warning" data-toggle="collapse" data-parent="#accordion2" href="#collapseOne">
-        Información Personal
-      </a>
-    </div>
-    <div id="collapseOne" class="accordion-body collapse">
-      <div class="accordion-inner">
-          <?php 
+        <div class="accordion" id="accordion2"><!-- accordion historial -->
+            <div class="accordion-group"><!-- informacion personal del paciente-->
+                 <?php 
 		  // muestra los detalles de paciente
-		  include ("informacionPaciente.php"); ?>
-      </div>
-    </div>
-  </div>
-  <div class="accordion-group">
-  
-    <div class="accordion-heading">
-      <a class="btn btn-large btn-block btn-warning" data-toggle="collapse" data-parent="#accordion2" href="#collapseTwo">
-        Información Médica
-      </a>
-    </div>
-    <div id="collapseTwo" class="accordion-body collapse">
-      <div class="accordion-inner">
-          <?php 
+		  include ("informacionPaciente.php"); 
+                  ?>
+            </div><!-- informacion personal del paciente-->
+            
+            <div class="accordion-group"><!-- informacion medica registrada-->
+                <?php 
 		  //muestra alergias y condiciones del paciente
 		  include ("informacionMedica.php"); 
-          mostrarAlergias($alergias);
-          mostrarCondiciones($condiciones); ?>
-      </div>
-    </div>
-  </div>
-</div>
+                ?>
+            </div><!-- informacion medica registrada-->
+        </div>
+    
     </div><!-- Fin tab historial-->
     
     <div class="tab-pane img-rounded" id="tabConsulta"><!-- tab consulta-->
 
-<div class="accordion" id="accordion3"><!-- accordion consulta-->
+        <div class="accordion" id="accordion3"><!-- accordion consulta-->
   
-    <div class="accordion-group"><!-- accordion diagnostico-->
-        <?php include('moduloDiagnostico.php'); ?>
-  </div><!-- accordion diagnostico
-  <div class="accordion-group"><!--accordion receta  -->
-    <?php include('moduloReceta.php'); ?>
-  </div>
-
-
-</div><!-- accordion consulta-->
+            <div class="accordion-group"><!-- accordion diagnostico-->
+            <?php include('moduloDiagnostico.php'); ?>
+            </div><!-- accordion diagnostico -->
+    
+            <div class="accordion-group"><!--accordion receta  -->
+            <?php include('moduloReceta.php'); ?>
+            </div>
         
-        
-        
-        
-        
+        </div><!-- accordion consulta-->
+      
     </div><!-- Fin del tab consulta-->
     
-  </div>
+
   </div><!-- fin contenido del panel-->
-</div>
-</div>
+</div><!--fin div contenido -->
+</div><!-- fin contenedor general -->
         
         
       
--- Capa_Vistas/Medico/AtencionPaciente/informacionPaciente.php
+++ Capa_Vistas/Medico/AtencionPaciente/informacionPaciente.php
@@ -1,10 +1,18 @@
-
-<form class="form-inline">
-  <div class="control-group">
-    <label class="control-label" for="Nombre">Nombre <input class="span5" type="text" id="Nombre" value="<?php echo"".$paciente['Nombre']." ".$paciente['Apellido_Paterno']." ".$paciente['Apellido_Materno'].""; ?>" disabled></label>
+  <div class="accordion-heading">
+  <a class="btn btn-large btn-block btn-warning" data-toggle="collapse" data-parent="#accordion2" href="#collapseOne">
+  Información Personal del Paciente
+  </a>
   </div>
-  <div class="control-group">
-  <label class="control-label" for="Fecha">Fecha de Nacimiento  <input type="datetime" class="uneditable-input" id="Fecha" value="<?php echo $paciente['Fecha_Nac']; ?>" disabled></label>
+  <div id="collapseOne" class="accordion-body collapse">
+  <div class="accordion-inner">
+    <form class="form-inline">
+    
+    <div class="control-group">
+    <label class="control-label" for="Nombre">Nombre <input class="span5" type="text" id="Nombre" value="<?php echo"".$paciente['Nombre']." ".$paciente['Apellido_Paterno']." ".$paciente['Apellido_Materno'].""; ?>" disabled></label>
+    </div>
+    
+    <div class="control-group">
+    <label class="control-label" for="Fecha">Fecha de Nacimiento  <input type="datetime" class="uneditable-input" id="Fecha" value="<?php echo $paciente['Fecha_Nac']; ?>" disabled></label>
     <label class="control-label" for="Sexo">Sexo  <input type="text" class="span2" id="Sexo" value="<?php if($paciente['Sexo']=='1')
 	{
 		echo "Masculino";
@@ -15,30 +23,34 @@
 	}; ?>" disabled></label>
     <label class="control-label" for="Peso">Peso <input type="text" class="span1" id="Peso" value="<?php echo $paciente['Peso']; ?>"></label>
     <label class="control-label" for="Altura">Altura <input type="text" class="span1" id="Altura" value="<?php echo $paciente['Altura']; ?>"></label>
-  </div>
+    </div>
+    
     <div class="control-group">
     <label class="control-label" for="Pais">Pais <input type="text" class="span1" id="Pais" value="Chile" disabled></label>
     <label class="control-label" for="Region">Region <input type="text" class="span2 inline" id="Region" value="<?php echo $region['Nombre']; ?>"></label>
     <label class="control-label" for="Comuna">Comuna  <input type="text" class="span2 inline" id="Comuna" value="<?php echo $comuna['Nombre']; ?>"></label>
+    </div>
     
-    
-  </div>
-  <div class="control-group">
+    <div class="control-group">
     <label class="control-label" for="Direccion">Dirección  <input type="text" class="span5" id="Direccion" value="<?php echo "".$paciente['Calle']." ".$paciente['Numero']." "; ?>"></label>
-  </div>
+    </div>
 
-  <div class="control-group">
+    <div class="control-group">
     <label class="control-label" for="Nacionalidad">Nacionalidad  <input type="text" class="span2" id="Nacionalidad" value="<?php echo $paciente['Nacionalidad']; ?>" disabled></label>
     <label class="control-label" for="Etnia">Etnia  <input type="text" class="span2" id="Etnia" value="<?php echo $etnia['Nombre']; ?>" disabled></label>
-  </div>
-  <div class="control-group">
+    </div>
+        
+    <div class="control-group">
     <label class="control-label" for="N_Celular N_Fijo">Telefonos  <input type="text" class="span2" id="N_Celular" value="<?php echo $paciente['N_Celular']; ?>">  <input type="text" class="span2" id="N_Fijo" value="<?php echo $paciente['N_Fijo']; ?>">  </label>
-  </div>
-  <div class="control-group">
+    </div>
+    
+    <div class="control-group">
     <label class="control-label" for="Isapre">Isapre  <input type="text" class="span2" id="Isapre" value="<?php echo $prevision['Nombre']; ?>" disabled></label>
+    </div>
+    
+    </form> 
   </div>
-</form> 
-
+  </div>
 
 
 
