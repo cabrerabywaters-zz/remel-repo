fd2c1e67a76d492c468927774e49c8c7cae006d3
José-Francisco González <pratiko2010@gmail.com>
2013-01-07 14:42:08 -0400

    arreglo de plazas

--- Capa_Controladores/medicosHasPlazasInstituciones.php
+++ Capa_Controladores/medicosHasPlazasInstituciones.php
@@ -9,19 +9,15 @@ class MedicosHasPlazasInstituciones {
 
     public static function PlazasPorIDMedico($idMedico) {
         
-
+        //print_r($idMedico);
         $queryString = "Select idPlaza, Nombre from 
                 Plazas_Instituciones where idPlaza in  (Select id_plaza_institucion  from 
-                Medico_has_Plaza_Institucion where id_medico='".$idMedico ."' 
+                Medico_has_Plaza_Institucion where id_medico='".$idMedico[0] ."' 
                     and id_plaza_institucion   in   (Select 
                                 Plazas_Instituciones_idPlaza  from Prestadores_Salud ));";
-
         $resultado = CallQuery($queryString);
-
-                
         while($row = $resultado->fetch_assoc()){
 		$instituciones[] = $row;
-                
 	}
 
 	return $instituciones;
