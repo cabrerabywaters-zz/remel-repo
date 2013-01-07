c2196b534a06916679bfeec2d7b958cd91117386
Leonardo Hidalgo <leonardo@gmail.com>
2013-01-07 17:49:38 -0300

    despliega datos correctamente

--- Capa_Datos/llamarQuery.php
+++ Capa_Datos/llamarQuery.php
@@ -3,6 +3,7 @@
 function CallQuery($queryString){
 		include('dbconfig.php');
 		$mysqlCon = new mysqli($servidor,$nombre_usuario,$contrasena,$base_de_datos);
+		$mysqlCon->set_charset("utf8");
 
 		if($mysqlCon->errno) {
 			printf("Conexion fallida: %s\n", $mysqli->connect_error);
