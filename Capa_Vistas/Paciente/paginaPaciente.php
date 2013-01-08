2d5f7a2d13c653336bfb6b6d28a9c5dd739f0b99
José-Francisco González <pratiko2010@gmail.com>
2013-01-08 17:31:15 -0400

    pagina funcional (opcion de salir y header)

--- Capa_Vistas/Paciente/paginaPaciente.php
+++ Capa_Vistas/Paciente/paginaPaciente.php
@@ -1,14 +1,18 @@
-<!--
-To change this template, choose Tools | Templates
-and open the template in the editor.
--->
+<?php
+include '/../../ajax/sessionCheck.php';
+
+iniciarCookie();
+verificarIP();
+include('/../pacienteHeader.php');
+?>
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title></title>
-    </head>
+    </head> 
     <body>
   <a href="../Medico/logout.php" role="button" class="btn btn-large btn-block btn-danger">Salir</a>
+  <? echo $Paciente['Foto'];?>
     </body>
 </html>
