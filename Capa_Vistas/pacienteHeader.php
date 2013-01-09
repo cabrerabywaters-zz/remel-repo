e8633e9a980b2b6def1ae68c755383b0752e2d32
José-Francisco González <pratiko2010@gmail.com>
2013-01-09 11:35:53 -0400

    header paciente

--- Capa_Vistas/pacienteHeader.php
+++ Capa_Vistas/pacienteHeader.php
@@ -163,7 +163,23 @@
                 </div>
             </div>
     </div>
-    <? echo 'semen';?>
+    <ul class="nav nav-tabs img-rounded" style="background: rgb(242,245,246); /* Old browsers */
+background: -moz-linear-gradient(45deg,  rgba(242,245,246,1) 0%, rgba(227,234,237,1) 37%, rgba(200,215,220,1) 100%); /* FF3.6+ */
+background: -webkit-gradient(linear, left bottom, right top, color-stop(0%,rgba(242,245,246,1)), color-stop(37%,rgba(227,234,237,1)), color-stop(100%,rgba(200,215,220,1))); /* Chrome,Safari4+ */
+background: -webkit-linear-gradient(45deg,  rgba(242,245,246,1) 0%,rgba(227,234,237,1) 37%,rgba(200,215,220,1) 100%); /* Chrome10+,Safari5.1+ */
+background: -o-linear-gradient(45deg,  rgba(242,245,246,1) 0%,rgba(227,234,237,1) 37%,rgba(200,215,220,1) 100%); /* Opera 11.10+ */
+background: -ms-linear-gradient(45deg,  rgba(242,245,246,1) 0%,rgba(227,234,237,1) 37%,rgba(200,215,220,1) 100%); /* IE10+ */
+background: linear-gradient(45deg,  rgba(242,245,246,1) 0%,rgba(227,234,237,1) 37%,rgba(200,215,220,1) 100%); /* W3C */
+filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#f2f5f6', endColorstr='#c8d7dc',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
+"><!-- barra de navegacion -->
+                    <li class="active img-rounded"><a href="#tabHistorial" data-toggle="tab">Información Personal</a></li>
+                    <li><a href="#tabConsulta" data-toggle="tab">Mis Diagnósticos</a></li>
+                      <li><a href="#" data-toggle="tab">Mis Recetas</a></li>
+                      <li><a href="#" data-toggle="tab">Historial Médico</a>
+                      <li class="pull-right"><a href="/Medico/logout.php">Salir</a></li>
+
+                    
+        </ul>
 
 
 
