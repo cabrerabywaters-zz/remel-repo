8e8ee4cf8208ff0fc21a21d9299406ba92a5c775
José-Francisco González <pratiko2010@gmail.com>
2013-01-09 11:36:20 -0400

    pagina paciente

--- Capa_Vistas/Paciente/paginaPaciente.php
+++ Capa_Vistas/Paciente/paginaPaciente.php
@@ -13,34 +13,29 @@ include('/../pacienteHeader.php');
     </head> 
     <body>
         
-        <ul class="nav nav-tabs img-rounded" style="background: rgb(242,245,246); /* Old browsers */
-background: -moz-linear-gradient(45deg,  rgba(242,245,246,1) 0%, rgba(227,234,237,1) 37%, rgba(200,215,220,1) 100%); /* FF3.6+ */
-background: -webkit-gradient(linear, left bottom, right top, color-stop(0%,rgba(242,245,246,1)), color-stop(37%,rgba(227,234,237,1)), color-stop(100%,rgba(200,215,220,1))); /* Chrome,Safari4+ */
-background: -webkit-linear-gradient(45deg,  rgba(242,245,246,1) 0%,rgba(227,234,237,1) 37%,rgba(200,215,220,1) 100%); /* Chrome10+,Safari5.1+ */
-background: -o-linear-gradient(45deg,  rgba(242,245,246,1) 0%,rgba(227,234,237,1) 37%,rgba(200,215,220,1) 100%); /* Opera 11.10+ */
-background: -ms-linear-gradient(45deg,  rgba(242,245,246,1) 0%,rgba(227,234,237,1) 37%,rgba(200,215,220,1) 100%); /* IE10+ */
-background: linear-gradient(45deg,  rgba(242,245,246,1) 0%,rgba(227,234,237,1) 37%,rgba(200,215,220,1) 100%); /* W3C */
-filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#f2f5f6', endColorstr='#c8d7dc',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
-"><!-- barra de navegacion -->
-                    <li class="active img-rounded"><a href="#tabHistorial" data-toggle="tab">Información Personal</a></li>
-                    <li><a href="#tabConsulta" data-toggle="tab">Mis Diagnósticos</a></li>
-                      <li><a href="#" data-toggle="tab">Mis Recetas</a></li>
-                      <li><a href="#" data-toggle="tab">Mis Médicos</a></li>
-                      <li><a href="#" data-toggle="tab">Mis Instituciones</a>
-                    <!--<li class="dropdown pull-right">
-                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                            Cuenta <b class="caret"></b>
-                        </a>
-                        <ul class="dropdown-menu">
-                        <!-- links -->
-               <!--         <li><a href="../doctorIndex.php">Editar Información Personal</a></li>
-                        <li><a href="../logout.php">Salir</a></li>
-                        </ul>
-                    </li>-->
-                      <li class="pull-right"><a href="../logout.php">Salir</a></li>
-
-                    
-                </ul>
-  <!--<?php //echo $Paciente['Foto'];?>-->
+        
+  
+  
+    <div class="tab-content"><!-- contenido del panel 1-->
+    
+    <div class="tab-pane active img-rounded" id="tabHistorial"><!-- tab Historial-->
+      
+        <div class="accordion" id="accordion2"><!-- accordion historial -->
+            <div class="accordion-group"><!-- informacion personal del paciente-->
+                 <?php 
+		  // muestra los detalles de paciente
+		  include ("informacionPacientePaciente.php"); 
+                  ?>
+            </div><!-- informacion personal del paciente-->
+            
+            <div class="accordion-group"><!-- informacion medica registrada-->
+                <?php 
+		  //muestra alergias y condiciones del paciente
+		  include ("informacionMedicaPaciente.php"); 
+                ?>
+            </div><!-- informacion medica registrada-->
+        </div>
+    
+    </div><!-- Fin tab historial-->
     </body>
 </html>
