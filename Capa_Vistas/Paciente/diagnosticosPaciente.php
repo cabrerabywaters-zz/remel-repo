800eb0275f4f4653ef865f48ca92a964c522a495
José-Francisco González <pratiko2010@gmail.com>
2013-01-09 16:57:09 -0400

    tabla diagnosticos

--- Capa_Vistas/Paciente/diagnosticosPaciente.php
+++ Capa_Vistas/Paciente/diagnosticosPaciente.php
@@ -1,12 +1,31 @@
 
-  <a class="active btn btn-large btn-block " data-toggle="collapse" data-parent="#accordion2" href="#collapsethree">
-  Mis Diagnosticos
+<a class="active btn btn-large btn-block " data-toggle="collapse" data-parent="#accordion2" href="#collapsethree">
+    Mis Diagnosticos
 
-  </a>
-  <div class="accordion-inner">
-      <?php
-        var_dump($diagnosticosPaciente);
-      ?>
-      
-  </div>
+</a>
+<div class="accordion-inner">
+    <?php
+    var_dump($diagnosticosPaciente);
+
+    echo'
+  <div class="row">
+  <div class="span5" id="diagnosticos">
+  <table>
+                <tr><td>
+   <table class="table table-hover">';
+
+    foreach ($diagnosticosPaciente as $datos => $dato) {
+        echo"	<tr>";
+        foreach ($dato as $valor) {
+            echo '<td>';
+            echo $valor;
+            echo '</td>';
+        }
+
+            echo "</tr>";
+    }
+    echo '</table>';
+    ?>
+
+</div>
 
