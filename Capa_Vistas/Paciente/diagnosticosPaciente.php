423859e34459ddbc490a13c8798f4e4c36c48850
Leonardo Hidalgo <leonardo@gmail.com>
2013-01-09 15:10:19 -0300

    despliege de datos en paciente

--- Capa_Vistas/Paciente/diagnosticosPaciente.php
+++ Capa_Vistas/Paciente/diagnosticosPaciente.php
@@ -1,5 +1,12 @@
-<?php
 
+<div class="accordion-heading">
+  <a class="btn btn-large btn-block " data-toggle="collapse" data-parent="#accordion2" href="#collapseOne">
+  Mis Diagnosticos
+  </a>
+  </div>
+  <div id="collapseOne" class="accordion-body collapse">
+  <div class="accordion-inner">
+aqui va algo 
+    </div>
+  </div>
 
-
-?>
