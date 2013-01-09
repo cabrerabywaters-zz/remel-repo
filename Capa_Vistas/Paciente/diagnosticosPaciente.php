ce9feb1e718016603f24a3f99a890179cda58bac
José-Francisco González <pratiko2010@gmail.com>
2013-01-09 16:41:05 -0400

    diagnosticos paciente

--- Capa_Vistas/Paciente/diagnosticosPaciente.php
+++ Capa_Vistas/Paciente/diagnosticosPaciente.php
@@ -1,12 +1,12 @@
 
-<div class="accordion-heading">
-  <a class="btn btn-large btn-block " data-toggle="collapse" data-parent="#accordion2" href="#collapsethree">
+  <a class="active btn btn-large btn-block " data-toggle="collapse" data-parent="#accordion2" href="#collapsethree">
   Mis Diagnosticos
+
   </a>
- </div>
-  <div id="collapsethree" class="accordion-body collapse">
   <div class="accordion-inner">
-aqui va algo 
-    </div>
+      <?php
+        var_dump($diagnosticosPaciente);
+      ?>
+      
   </div>
 
