91e5ff55e32059a9ef3e4be18217f59aadfab7f5
Leonardo Hidalgo <leonardo@gmail.com>
2013-01-09 14:23:36 -0300

    Reparacion del Query de busqueda

--- Capa_Controladores/paciente.php
+++ Capa_Controladores/paciente.php
@@ -149,14 +149,16 @@ AND Pacientes.idPaciente=".$idPaciente."";
     
     public static function R_DiagnosticoPaciente($idPaciente) {
     	$queryString="SELECT Diagnosticos.Nombre, Consulta.Fecha, Personas.Nombre, Personas.Apellido_Paterno
-                      FROM Pacientes, Medicos, Consulta, Pacientes, Historiales_medicos, Diagnosticos
-                      WHERE Pacientes.idPaciente = Paciente_has_Condiciones.Paciente_idPaciente
-                      AND Pacientes.idPaciente = $idPaciente
-                      AND Pacientes.idPaciente = Consulta.idPaciente
-                      AND Consulta.Id_consulta = Historiales_medicos.Consulta_Id_consulta
-                      AND Historiales_medicos.Diagnosticos_idDiagnostico = Diagnosticos.idDiagnostico
-                      AND Consulta.Medicos_idMedico = Medicos.idMedico
-                      AND Medicos.Personas_RUN = Personas.RUN";
+						FROM Personas, Medicos, Consulta, Pacientes, Historiales_medicos, Diagnosticos, 
+						Paciente_has_Condiciones
+						WHERE Pacientes.idPaciente = Paciente_has_Condiciones.Paciente_idPaciente
+						AND Pacientes.idPaciente = $idPaciente
+						AND Pacientes.idPaciente = Consulta.Pacientes_idPaciente
+						AND Consulta.Id_consulta = Historiales_medicos.Consulta_Id_consulta
+						AND Historiales_medicos.Diagnosticos_idDiagnostico = Diagnosticos.idDiagnostico
+						AND Consulta.Medicos_idMedico = Medicos.idMedico
+						AND Medicos.Personas_RUN = Personas.RUN";
+					  
 
         $result = CallQuery($queryString);
 	    $resultArray = array();
