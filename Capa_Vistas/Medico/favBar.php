927adc75b57b71c05834ee1adbc78be8d6384ec9
César González Barreto <cgonzb@gmail.com>
2013-01-10 13:27:04 -0300

    archivo favBar

---
+++ Capa_Vistas/Medico/favBar.php
@@ -0,0 +1,11 @@
+<div id="favBar" class="span3 pull-right img-rounded" style="background-color: #B0BED9"><!-- barra de favoritos -->
+      <button class="btn btn-large btn-block btn-inverse"><i class="icon-star icon-white"></i> Mis Favoritos</button>
+
+
+
+
+
+
+
+
+</div><!-- fin de la barra de favoritos -->
