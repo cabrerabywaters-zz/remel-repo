3f4f1ced854b8b23e230fd1bb2f68940406bbabc
César González Barreto <cgonzb@gmail.com>
2013-01-11 16:51:49 -0300

    pills de favoritos se pueden eliminar de la barra
    falta la parte de eliminarlos de la bbdd y luego el comportamiento de
    agregación a la receta

--- Capa_Vistas/Medico/favBar.php
+++ Capa_Vistas/Medico/favBar.php
@@ -1,39 +1,50 @@
 <div id="favBar" class="span3 pull-right img-rounded" style="background-color: #B0BED9"><!-- barra de favoritos -->
+    <div class="row-fluid show-grid">
       <button class="btn btn-large btn-block btn-primary"><i class="icon-star icon-white"></i> Mis Favoritos</button>
       
       <!-- diagnosticos Favoritos -->
       <button type="button" class="btn btn-success btn-block" data-toggle="collapse" data-target="#diagnosticosFav">
-           <i class="icon-circle-arrow-up icon-white"></i> Diagnosticos
+           <i class="icon-circle-arrow-up icon-white" rel="tooltip" title="Ocultar"></i> Diagnosticos
       </button>
-      <div id="diagnosticosFav" class="collapse in span2">
+      
+      <div id="diagnosticosFav" class="collapse in">
+          <div class="span9 offset1">
+              
           <span class="label label-info"><!-- pill diagnosticoFav 1 -->
               Diagnostico ej 
-              <a href="#ejemplo"> <i class="icon-plus-sign icon-white"></i></a>
-              <a href="#ejemplo"> <i class="icon-remove-sign icon-white"></i></a>
+              <a href="#agregarFav" rel="tooltip" title="Agregar"> <i class="icon-plus-sign icon-white"></i></a><!-- agregar favorito seleccionado -->
+              <a href="#borrarFav" rel="tooltip" title="Eliminar"> <i class="icon-remove-sign icon-white"></i></a><!-- eliminar de favoritos -->
           </span><!-- end pill diagnosticoFav 1-->
           <span class="label label-info"><!-- pill diagnosticoFav 1 -->
               Diagnostico ej 
-              <a href="#ejemplo"> <i class="icon-plus-sign icon-white"></i></a>
-              <a href="#ejemplo"> <i class="icon-remove-sign icon-white"></i></a>
+              <a href="#agregarFav" rel="tooltip" title="Agregar"> <i class="icon-plus-sign icon-white"></i></a><!-- agregar favorito seleccionado -->
+              <a href="#borrarFav" rel="tooltip" title="Eliminar"> <i class="icon-remove-sign icon-white"></i></a><!-- eliminar de favoritos -->
           </span><!-- end pill diagnosticoFav 1-->
+          
+          </div>
       </div>
       <!-- fin diagnosticos favoritos-->
       
       <!-- medicamentos favoritos -->
       <button type="button" class="btn btn-success btn-block" data-toggle="collapse" data-target="#medicamentosFav">
-          <i class="icon-circle-arrow-up icon-white"></i> Medicamentos 
+          <i class="icon-circle-arrow-up icon-white" rel="tooltip" title="Ocultar"></i> Medicamentos
       </button>
-      <div id="medicamentosFav" class="collapse in span2">
+      
+      <div id="medicamentosFav" class="collapse in ">
+       <div class="span9 offset1">
+           
           <span class="label label-info"><!-- pill medicamentoFav 1 -->
               Medicamento ej 
-              <a href="#ejemplo"> <i class="icon-plus-sign icon-white"></i></a>
-              <a href="#ejemplo"> <i class="icon-remove-sign icon-white"></i></a>
+              <a href="#agregarFav" rel="tooltip" title="Agregar"> <i class="icon-plus-sign icon-white"></i></a><!-- agregar favorito seleccionado -->
+              <a href="#borrarFav" rel="tooltip" title="Eliminar"> <i class="icon-remove-sign icon-white"></i></a><!-- eliminar de favoritos -->
           </span><!-- pill medicamentoFav 1 -->
            <span class="label label-info"><!-- pill medicamentoFav 1 -->
               Medicamento ej 
-              <a href="#ejemplo"> <i class="icon-plus-sign icon-white"></i></a>
-              <a href="#ejemplo"> <i class="icon-remove-sign icon-white"></i></a>
-          </span><!-- pill medicamentoFav 1 -->
+              <a href="#agregarFav" rel="tooltip" title="Agregar"> <i class="icon-plus-sign icon-white"></i></a><!-- agregar favorito seleccionado -->
+              <a href="#borrarFav" rel="tooltip" title="Eliminar"> <i class="icon-remove-sign icon-white"></i></a><!-- eliminar de favoritos -->
+           </span><!-- pill medicamentoFav 1 -->
+       
+       </div>
       </div>
       <!-- fin medicamentos favoritos -->
 
@@ -41,7 +52,7 @@
 
 
 
-
+    </div>
 </div><!-- fin de la barra de favoritos -->
 
 <script>
@@ -51,14 +62,51 @@
     */ 
    
    $('#diagnosticosFav').on('hide',function(){
-       $('button[data-target="#diagnosticosFav"] i').removeClass("icon-circle-arrow-up").addClass("icon-circle-arrow-down");})
+       $('button[data-target="#diagnosticosFav"] i')
+            .removeClass("icon-circle-arrow-up")
+            .addClass("icon-circle-arrow-down")
+            .attr('title','Mostrar')
+            ;})
    $('#diagnosticosFav').on('show',function(){
-       $('button[data-target="#diagnosticosFav"] i').removeClass("icon-circle-arrow-down").addClass("icon-circle-arrow-up");})
+        var diagnosticosFavNum = $('#diagnosticosFav').children().children('span').size();
+       $('button[data-target="#diagnosticosFav"] i')
+            .removeClass("icon-circle-arrow-down")
+            .addClass("icon-circle-arrow-up")
+            .attr('title','Ocultar')
+            ;})
    $('#medicamentosFav').on('hide',function(){
-       $('button[data-target="#medicamentosFav"] i').removeClass("icon-circle-arrow-up").addClass("icon-circle-arrow-down");})
+       $('button[data-target="#medicamentosFav"] i')
+            .removeClass("icon-circle-arrow-up")
+            .addClass("icon-circle-arrow-down")
+            .attr('title','Mostrar')
+            ;})
    $('#medicamentosFav').on('show',function(){
-       $('button[data-target="#medicamentosFav"] i').removeClass("icon-circle-arrow-down").addClass("icon-circle-arrow-up");})
+       $('button[data-target="#medicamentosFav"] i')
+            .removeClass("icon-circle-arrow-down")
+            .addClass("icon-circle-arrow-up")
+            .attr('title','Ocultar')
+            ;})
      
-       
+   /*
+    * eliminación de un elemento de la barra favoritos
+    * haciendo click en el boton eliminar de favoritos
+    *+tooltip correspondientes por boton
+    */
+   $('a[rel="tooltip"]').tooltip({placement:"right"});
+   
+   
+   $('a[href="#borrarFav"]').click(function(){
+       /**
+        * funcion que elimina de favoritos
+        * se debe primero eliminar de la bbdd vía ajax
+        * luego se elimina del DOM
+        */
+       $(this).parent('span').remove();
+   });
 
+   
+   /*
+    * agregar un elemento a la receta al hacer click
+    * desde la barra de favori
+    */
 </script>
