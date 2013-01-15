eb3b88dca1cc9df39e35986df40a31f76fa68d3a
César González Barreto <cgonzb@gmail.com>
2013-01-15 17:10:09 -0300

    tweaks visuales de la emision de la receta

--- Capa_Vistas/Medico/arsenalBar.php
+++ Capa_Vistas/Medico/arsenalBar.php
@@ -2,21 +2,21 @@
 <div class="row-fluid show-grid img-rounded" id="arsenalBar" style="background-color: #7aba7b"> 
     <button class="btn btn-block" disabled>
         <i class="icon-list"></i> Arsenal
-        <a href="#" class="closeBar"><i class="icon-remove-sign pull-right"></i></a>
+        <a href="#" class="closeBar"><i class="icon-remove pull-right"></i></a>
       </button>
     <div id="medicamentosFav">
-       <div class="span9 offset1">
+       <div class="span10 offset1">
            
-          <span class="label label-info"><!-- pill medicamentoFav 1 -->
-              Medicamento ej 
-              <a href="#agregarFav" rel="tooltip" title="Agregar"> <i class="icon-plus-sign icon-white"></i></a><!-- agregar desde el arsenal seleccionado -->
-              <a href="#borrarFav" rel="tooltip" title="Eliminar"> <i class="icon-star icon-white"></i></a><!-- eliminar de favoritos -->
-          </span><!-- pill medicamentoFav 1 -->
-           <span class="label label-info"><!-- pill medicamentoFav 1 -->
-              Medicamento ej 
-              <a href="#agregarFav" rel="tooltip" title="Agregar"> <i class="icon-plus-sign icon-white"></i></a><!-- agregar favorito seleccionado -->
-              <a href="#borrarFav" rel="tooltip" title="Eliminar"> <i class="icon-star icon-white"></i></a><!-- eliminar de favoritos -->
-           </span><!-- pill medicamentoFav 1 -->
+          <div class="alert alert-warning"><!-- pill medicamentoFav 1 -->
+              <strong>Medicamento Arsenal ej</strong>
+              <a href="#borrarFav" rel="tooltip" title="Agregar a Favoritos"> <i class="icon-star pull-right"></i></a><!-- eliminar de favoritos --> 
+              <a href="#" rel="tooltip" title="Agregar a Receta"> <i class="detalleMedicamento icon-plus pull-right"></i></a><!-- agregar favorito seleccionado -->
+          </div><!-- end pill diagnosticoFav 1-->
+          <div class="alert alert-warning"><!-- pill medicamentoFav 1 -->
+              <strong>Medicamento Arsenal ej</strong>
+              <a href="#borrarFav" rel="tooltip" title="Agregar a Favoritos"> <i class="icon-star pull-right"></i></a><!-- eliminar de favoritos --> 
+              <a href="#" rel="tooltip" title="Agregar a Receta"> <i class="detalleMedicamento icon-plus pull-right"></i></a><!-- agregar favorito seleccionado -->
+          </div><!-- end pill diagnosticoFav 1-->
        
        </div>
       </div>
