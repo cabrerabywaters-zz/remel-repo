1d670d53a422e5f6f718275b9361d6717051fb71
César González Barreto <cgonzb@gmail.com>
2013-01-16 11:13:21 -0300

    orden de codigo

--- Capa_Vistas/Medico/favBar.php
+++ Capa_Vistas/Medico/favBar.php
@@ -80,11 +80,13 @@
     </div><!-- fin de la barra de favoritos -->
 
 <div id="modalDetalleMedicamento" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-header">
+    
+    <div class="modal-header"><!-- titulo del modal (nombre del medicamento) -->
         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
         <h3 id="myModalLabel">Paracetamol</h3>
-    </div>
-    <div class="modal-body">
+    </div><!-- titulo del modal (nombre del medicamento) -->
+    
+    <div class="modal-body"><!-- contenido del modal (indicaciones de día frecuencia etc)-->
         <div class="span3"><img src="../../../imgs/paracetamol.jpg" style="width:60%" ></div>
         <p>El paracetamol (DCI) o acetaminofén (acetaminofeno) es un fármaco con propiedades analgésicas, sin propiedades antiinflamatorias clínicamente significativas. Actúa inhibiendo la síntesis de prostaglandinas, mediadores celulares responsables de la aparición del dolor. Además, tiene efectos antipiréticos.</p>
         <p>Cantidad: <input type="text" placeholder="Indique Cantidad">Miligramos (mg)</p>
@@ -92,11 +94,13 @@
         <p>Por :<input type="text" placeholder="periodo">Dias</p>
         <p>Comentario: </p>
         <center> <textarea rows="2" style="width:90%"></textarea></center>
-    </div>
-    <div class="modal-footer">
+    </div><!-- contenido del modal (indicaciones de día frecuencia etc)-->
+    
+    <div class="modal-footer"><!-- acciones del modal (cancerlar, agregar medicamento etc)-->
         <button class="btn" data-dismiss="modal" aria-hidden="true">Cancelar</button>
         <a href="#" role="button" class="btn btn-warning">Prescribir</a>
     </div>
+
 </div><!-- fin popup informacion del medicamento -->  
     
     
