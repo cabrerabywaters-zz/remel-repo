5f011fa0536dd998d5b7c91ad29015c584bf88e3
César González Barreto <cgonzb@gmail.com>
2013-01-16 11:13:41 -0300

    movido modal de medicamento a favBar

--- Capa_Vistas/Medico/AtencionPaciente/moduloReceta.php
+++ Capa_Vistas/Medico/AtencionPaciente/moduloReceta.php
@@ -1,5 +1,4 @@
-<!-- moduloReceta.php -->
-<!--
+<!-- moduloReceta.php
 Contiente el modulo de receta en la atencion paciente
 incluyendo el buscador predictivo de medicamento
 y el popup que muestra el detalle del medicamento
@@ -53,29 +52,6 @@ y el popup que muestra el detalle del medicamento
         
      </div>
 </div>
-
-
-
-
-<div id="myModal2" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-        <h3 id="myModalLabel">Paracetamol</h3>
-    </div>
-    <div class="modal-body">
-        <div class="span3"><img src="../../../imgs/paracetamol.jpg" style="width:60%" ></div>
-        <p>El paracetamol (DCI) o acetaminofén (acetaminofeno) es un fármaco con propiedades analgésicas, sin propiedades antiinflamatorias clínicamente significativas. Actúa inhibiendo la síntesis de prostaglandinas, mediadores celulares responsables de la aparición del dolor. Además, tiene efectos antipiréticos.</p>
-        <p>Cantidad: <input type="text" placeholder="Indique Cantidad">Miligramos (mg)</p>
-        <p>Cada :<input type="text" placeholder="frequencia">Horas (hrs)</p>
-        <p>Por :<input type="text" placeholder="periodo">Dias</p>
-        <p>Comentario: </p>
-        <center> <textarea rows="2" style="width:90%"></textarea></center>
-    </div>
-    <div class="modal-footer">
-        <button class="btn" data-dismiss="modal" aria-hidden="true">Cancelar</button>
-        <a href="#" role="button" class="btn btn-warning">Prescribir</a>
-    </div>
-</div><!-- fin popup informacion del medicamento -->
 
 <!--JS de funcion verificacion de contraindicaciones-->
 <script>
