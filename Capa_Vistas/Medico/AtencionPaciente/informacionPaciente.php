b7d17c97bd55b7da553410943c4720d88e2261f6
Maximiano <maxsilva@alumnos.uai.cl>
2013-01-16 17:17:46 -0300

    boton

--- Capa_Vistas/Medico/AtencionPaciente/informacionPaciente.php
+++ Capa_Vistas/Medico/AtencionPaciente/informacionPaciente.php
@@ -6,15 +6,15 @@
   </div>
   <div id="collapseOne" class="accordion-body collapse">
   <div class="accordion-inner">
-   <center> <div style="width: 50%; ;"><form class="form-inline" >
+   <center> <div style="width: 50%; ;"><form id="arbol" class="form-inline" >
     
     <div class="control-group">
-    <label class="control-label" for="Nombre" ><strong>Nombre </strong> <br><input style="text-align:center;" class="span6"  type="text" id="Nombre" value="<?php echo"".$paciente['Nombre']." ".$paciente['Apellido_Paterno']." ".$paciente['Apellido_Materno'].""; ?>" disabled></label>
+    <label class="control-label" for="Nombre" ><strong>Nombre </strong> <br><input style="text-align:center;" class="span6" type="text" id="Nombre" value="<?php echo"".$paciente['Nombre']." ".$paciente['Apellido_Paterno']." ".$paciente['Apellido_Materno'].""; ?>" disabled></label>
     </div>
     
     <div class="control-group">
     <label class="control-label" for="Fecha" > <strong>Fecha de Nacimiento </strong> <br> <input style="text-align:center;" type="datetime" class="span3 uneditable-input" id="Fecha" value="<?php echo $paciente['Fecha_Nac']; ?>" disabled></label>&nbsp
-    <label class="control-label" for="Sexo"><strong>Sexo </strong> <br>  <input style="text-align:center;" type="text" class="span3" id="Sexo" value="<?php if($paciente['Sexo']=='1')
+    <label class="control-label" for="Sexo"><strong>Sexo </strong> <br>  <input style="text-align:center;" type="text" class="span3 edicion" id="Sexo" value="<?php if($paciente['Sexo']=='1')
 	{
 		echo "Masculino";
 	}
@@ -23,18 +23,18 @@
 	echo "Femenino";	
 	}; ?>" disabled></label>
     <br> <br>
-    <label class="control-label" for="Peso"><strong>Peso [Kg]  </strong><br> <input style="text-align:center;" type="text" class="span3" id="Peso" value="<?php echo $paciente['Peso']; ?>"></label>
-    <label class="control-label" for="Altura"><strong>Altura [Cm] </strong><br><input style="text-align:center;" type="text" class="span3" id="Altura" value="<?php echo $paciente['altura']; ?>"></label>
+    <label class="control-label" for="Peso"><strong>Peso [Kg]  </strong><br> <input style="text-align:center;" type="text" class="span3 edicion"  id="Peso" value="<?php echo $paciente['Peso']; ?>"></label>
+    <label class="control-label" for="Altura"><strong>Altura [Cm] </strong><br><input style="text-align:center;" type="text" class="span3 edicion" id="Altura" value="<?php echo $paciente['altura']; ?>"></label>
     </div>
      <br> 
     <div class="control-group">
     <label class="control-label" for="Pais"><strong>País </strong> <br><input style="text-align:center;" type="text" class="span2" id="Pais" value="Chile" disabled></label>
-    <label class="control-label" for="Region"><strong>Región </strong><br> <input style="text-align:center;" type="text" class="span2 inline" id="Region" value="<?php echo $region['Nombre']; ?>"></label>
-    <label class="control-label" for="Comuna"><strong>Comuna </strong> <br> <input style="text-align:center;" type="text" class="span2 inline" id="Comuna" value="<?php echo $comuna['Nombre']; ?>"></label>
+    <label class="control-label" for="Region"><strong>Región </strong><br> <input style="text-align:center;" type="text" class="span2 inline edicion" id="Region" value="<?php echo $region['Nombre']; ?>"></label>
+    <label class="control-label" for="Comuna"><strong>Comuna </strong> <br> <input style="text-align:center;" type="text" class="span2 inline edicion" id="Comuna" value="<?php echo $comuna['Nombre']; ?>"></label>
     </div>
     
     <div class="control-group">
-    <label class="control-label" for="Direccion"><strong>Dirección  </strong> <br> <input style="text-align:center;" type="text" class="span6" id="Direccion" value="<?php echo "".$paciente['Calle']." ".$paciente['Numero']." "; ?>"></label>
+    <label class="control-label" for="Direccion"><strong>Dirección  </strong> <br> <input style="text-align:center;" type="text" class="span6 edicion" id="Direccion" value="<?php echo "".$paciente['Calle']." ".$paciente['Numero']." "; ?>"></label>
     </div>
  
     <div class="control-group">
@@ -43,36 +43,57 @@
     </div>
        
     <div class="control-group">
-    <label class="control-label" for="N_Celular N_Fijo"><strong>Teléfonos </strong> <br> <input style="text-align:center;" type="text" class="span3" id="N_Celular" value="<?php echo $paciente['N_Celular']; ?>">  <input style="text-align:center;" type="text" class="span3" id="N_Fijo" value="<?php echo $paciente['n_fijo']; ?>">  </label>
+    <label class="control-label" for="N_Celular N_Fijo"><strong>Teléfonos </strong> <br> <input style="text-align:center;" type="text" class="span3 edicion" id="N_Celular" value="<?php echo $paciente['N_Celular']; ?>">  <input style="text-align:center;" type="text" class="span3 edicion" id="N_Fijo" value="<?php echo $paciente['n_fijo']; ?>">  </label>
     </div>
      
+    
     <div class="control-group">
     <label class="control-label" for="Isapre"><strong>Isapre </strong> <br> <input style="text-align:center;" type="text" class="span6" id="Isapre" value="<?php echo $prevision['Nombre']; ?>" disabled></label>
     </div>
     
     </form></div> </center>
+      
+      
+      
   </div>
   </div>
 
 
+<html>
+<head>
+    <link rel="stylesheet" href="http://code.jquery.com/ui/1.8.24/themes/base/jquery-ui.css">
+    <script src="http://code.jquery.com/jquery-1.8.2.js"></script>
+    <script src="http://code.jquery.com/ui/1.8.24/jquery-ui.js"></script>
+</head>
+<body>
+ 
+<center><button id="guardar" >Guardar</button></center>
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+<script> 
+    $('#guardar').hide();
+    $('.edicion').change(function() {
+                $('#guardar').show();
+    });
+    $('#guardar').click(function() {
+                $("#guardar").hide();
+                var editado = $("#arbol").serialize();
+                alert(editado);
+                $.ajax({
+                      url:'../../../ajax/algo.php',
+                      data: editado,
+                      type: 'post',
+                      success: function(output){
+                        var data = jQuery.parseJSON(output);
+                        
+                        $('#Sexo').html(data['sexo']);
+                        $('#Peso').html(data['peso']);
+                        $('#Altura').html(data['altura']);
+                        $('#Direccion').html(data['direccion']);
+                        $('#Comuna').html(data['comuna']);
+                        
+                      }
+                });
+           });
+    
+</script>
---
+++ ajax/actualizarDatosPaciente.php
@@ -0,0 +1,8 @@
+<?php
+
+include_once(dirname(__FILE__) . '/../Capa_Controladores/paciente.php');
+
+
+
+
+?>
