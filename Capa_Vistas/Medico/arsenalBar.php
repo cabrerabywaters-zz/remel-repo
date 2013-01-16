d3f618e7fd814c069f73027e5fcbea6105b7a361
César González Barreto <cgonzb@gmail.com>
2013-01-16 14:59:23 -0300

    modificados botones

--- Capa_Vistas/Medico/arsenalBar.php
+++ Capa_Vistas/Medico/arsenalBar.php
@@ -9,12 +9,12 @@
            
           <div class="alert alert-warning" medicamento="1"><!-- pill medicamentoFav 1 -->
               <strong>Medicamento Arsenal ej</strong>
-              <a href="#borrarFav" rel="tooltip" title="Agregar a Favoritos"> <i class="icon-star pull-right"></i></a><!-- eliminar de favoritos --> 
+              <a href="#" rel="tooltip" title="Agregar a Favoritos"> <i class="icon-star pull-right"></i></a><!-- eliminar de favoritos --> 
               <a href="#" rel="tooltip" title="Agregar a Receta"> <i class="detalleMedicamento icon-plus pull-right"></i></a><!-- agregar favorito seleccionado -->
           </div><!-- end pill diagnosticoFav 1-->
           <div class="alert alert-warning" medicamento="2"><!-- pill medicamentoFav 1 -->
               <strong>Medicamento Arsenal ej</strong>
-              <a href="#borrarFav" rel="tooltip" title="Agregar a Favoritos"> <i class="icon-star pull-right"></i></a><!-- eliminar de favoritos --> 
+              <a href="#" rel="tooltip" title="Agregar a Favoritos"> <i class="icon-star pull-right"></i></a><!-- eliminar de favoritos --> 
               <a href="#" rel="tooltip" title="Agregar a Receta"> <i class="detalleMedicamento icon-plus pull-right"></i></a><!-- agregar favorito seleccionado -->
           </div><!-- end pill diagnosticoFav 1-->
        
