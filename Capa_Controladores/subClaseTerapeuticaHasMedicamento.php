9720b53753313cb6b6015d4c3204ab1d8f5a291b
mrbungie <geroa220@gmail.com>
2013-01-17 10:29:22 -0300

    Arreglado multiselect (OJO CON LOS CONTROLADORES)

---
+++ Capa_Controladores/subClaseTerapeuticaHasMedicamento.php
@@ -0,0 +1,71 @@
+<?php 
+
+include_once(dirname(__FILE__).'/../Capa_Datos/generadorStringQuery.php');
+
+class SubClaseTerapeuticaHasMedicamento {
+
+    static $nombreTabla = "SubClases_Terapeuticas_has_Medicamentos";
+    static $nombreIdTabla = "SubClase_Terapeutica_idSubClase";    
+    
+    /**
+     * Insertar
+     * 
+     * Inserta una nueva entrada
+     * 
+     */
+    public static function Insertar() {
+    	$datosCreacion = array(
+                                array('Nombre',$_POST['nombre_sub_clase_terapeutica']),
+                                      );
+
+        $queryString = QueryStringAgregar($datosCreacion, self::$nombreTabla);
+        $query = CallQuery($queryString);
+    }
+
+    /**
+     * BorrarPorId
+     * 
+     * Borra una entrada segun su id, pasada por POST.
+     */
+    public static function BorrarPorId() {
+        $id = $_POST['id'];
+        $queryString = QueryStringBorrarPorId(self::$nombreTabla, self::$nombreIdTabla, $this->_id);
+        $query = CallQuery($queryString);
+    }
+    
+    /**
+     * Seleccionar
+     * 
+     * Esta funcion selecciona todas las entradas de una tabla
+     * con respecto a una condicion dada. Tambien es posible
+     * entregar un limite y un offset.
+     * 
+     * @param string $where
+     * @param int $limit
+     * @param int $offset
+     * @returns array $resultArray
+     */
+    public static function Seleccionar($where, $limit = 0, $offset = 0) {
+    	$atributosASeleccionar = array(
+					'Medicamento_idMedicamento'
+      );
+
+        $queryString = QueryStringSeleccionar($where, $atributosASeleccionar, self::$nombreTabla);
+
+	    if($limit != 0){
+	       $queryString = $queryString." LIMIT $limit";
+	    }
+	    if($offset != 0){
+		  $queryString = $queryString." OFFSET $offset ";
+	    }
+
+        $result = CallQuery($queryString);
+	    $resultArray = array();
+	    while($fila = $result->fetch_assoc()) {
+	       $resultArray[] = $fila['Medicamento_idMedicamento'];
+	    }
+	    return $resultArray;
+    }
+}
+
+?>
