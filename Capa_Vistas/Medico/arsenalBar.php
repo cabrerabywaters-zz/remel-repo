f6d4e483c57a28ddaeca2d99ca16a28ae8b5d3d8
César González Barreto <cgonzb@gmail.com>
2013-01-17 16:22:38 -0300

    clases en link y no en iconos

--- Capa_Vistas/Medico/arsenalBar.php
+++ Capa_Vistas/Medico/arsenalBar.php
@@ -7,15 +7,15 @@
     <div id="medicamentosFav">
        <div class="span10 offset1">
            
-          <div class="alert alert-warning" medicamento="1"><!-- pill medicamentoFav 1 -->
+          <div class="alert alert-warning" identificador="1"><!-- pill medicamentoFav 1 -->
               <strong>Medicamento Arsenal ej</strong>
               <a href="#" rel="tooltip" title="Agregar a Favoritos"> <i class="icon-star pull-right"></i></a><!-- eliminar de favoritos --> 
-              <a href="#" rel="tooltip" title="Agregar a Receta"> <i class="detalleMedicamento icon-plus pull-right"></i></a><!-- agregar favorito seleccionado -->
+              <a href="#" rel="tooltip" title="Agregar a Receta" class="detalleMedicamento"> <i class="icon-plus pull-right"></i></a><!-- agregar favorito seleccionado -->
           </div><!-- end pill diagnosticoFav 1-->
-          <div class="alert alert-warning" medicamento="2"><!-- pill medicamentoFav 1 -->
+          <div class="alert alert-warning" identificador="2"><!-- pill medicamentoFav 1 -->
               <strong>Medicamento Arsenal ej</strong>
               <a href="#" rel="tooltip" title="Agregar a Favoritos"> <i class="icon-star pull-right"></i></a><!-- eliminar de favoritos --> 
-              <a href="#" rel="tooltip" title="Agregar a Receta"> <i class="detalleMedicamento icon-plus pull-right"></i></a><!-- agregar favorito seleccionado -->
+              <a href="#" rel="tooltip" title="Agregar a Receta" class="detalleMedicamento"> <i class="icon-plus pull-right"></i></a><!-- agregar favorito seleccionado -->
           </div><!-- end pill diagnosticoFav 1-->
        
        </div>
