621b4748d403ff1b543fd2a7a3ecc75d84555d5e
Leonardo Hidalgo <leonardo@gmail.com>
2013-01-18 17:34:04 -0300

    creacion de funcion autocompletar

--- Capa_Vistas/Medico/AtencionPaciente/informacionMedica.php
+++ Capa_Vistas/Medico/AtencionPaciente/informacionMedica.php
@@ -37,10 +37,10 @@ function mostrarAlergias($alergias){
               echo '</tbody>
             </table>
             </tr></td><tfoot><tr><td> 
-			<form class="form-search">
+			<form class="form-search" id="buscar_alergia" method="post">
 			<div class="input-append">
   				<input class="span2" id="Alergias" type="text">
- 				 <button class="btn" type="button">Añadir</button>
+ 				 <button class="btn" type="button" id="boton_alergia">Añadir</button>
   			</div>
 			</form>
   </td></tr></tfoot></table></div>';
@@ -67,10 +67,10 @@ echo'
                 </tbody>
             </table>
             </tr></td><tfoot><tr><td> 
-			<form class="form-search">
+			<form class="form-search" id="buscar_condicion" method="post">
 			<div class="input-append">
   				<input class="span2" id="Condiciones" type="text">
-  				<button class="btn" type="button">Añadir</button></div>
+  				<button class="btn" type="button" id="boton_condiciones">Añadir</button></div>
 				</form></td></tr>	
 				</tfoot></table>
 			</div> 
@@ -83,34 +83,91 @@ echo'
     </div>
 <script>
 $( "#Condiciones" ).autocomplete({
-                                source: function( request, response ) {
-                                    $.ajax({
-                                        url: "../../../ajax/autocompleteCondiciones.php",
-                                        data: {
-                                            name_startsWith: request.term
-                                        },
-                                        type: "post",
-                                        success: function( data ) {
-                         
-                        
-                                            var output = jQuery.parseJSON(data);
-                       
-                                            response( $.map( output, function( item ) {
-                                                return {
-                                                    label: item
-                                                    // value: item.Nombre
-                                                }
-                                            }));
-                                        }
-                    
-                                    });
+                                /**
+                             * esta función genera el autocomplete para el campo de diagnostico (input)
+                             * al seleccionar y escribir 2 letras se ejecuta el ajax
+                             * busca en la base de datos en el archivo autocompleteDiagnostico.php
+                             * el jSon correspondiente a las coincidencias
+                             * 
+                             * Funcion select que ejecutará una accion cuando se devuelva
+                             */        
+                          source: function( request, response ){
+                                $.ajax({
+                                    url: "../../../ajax/autocompleteCondiciones.php",
+                                    data: {
+                                        name_startsWith: request.term
+                                    },
+                                    type: "post",
+                                    success: function( data ){
+                                        var output = jQuery.parseJSON(data);
+                                                                                
+                                        response( $.map( output, function( item ) {
+                                           return {
+                                               label: item.Nombre
+                                              ,id3 : item.idCondiciones
+                                            }
+                                            
+                                        })//end map
+                                        );  // end response
+                                    }//end success
+
+                                }); // end ajax
+                            },  // end source
+                           select: function(event, ui){
+                                    $('#Condiciones').removeAttr('idCondiciones').attr('idCondiciones',ui.item.id3)
+                                    $('#guardar_condiciones').removeAttr('disabled');
+                                    $('#boton_ondiciones').removeAttr('disabled');
                                 },
-                                minLength: 2,
-                                open: function() {
+                           minLength: 2,
+                           open: function() {
                                     $( this ).removeClass( "ui-corner-all" ).addClass( "ui-corner-top" );
-                                },
-                                close: function() {
+                                }, //end open
+                           close: function() {
                                     $( this ).removeClass( "ui-corner-top" ).addClass( "ui-corner-all" );
-                                }
-                            });
+                                } //end close
+                            });//autocompleteDiagnosticos
+$( "#Alergias" ).autocomplete({
+                                /**
+                             * esta función genera el autocomplete para el campo de diagnostico (input)
+                             * al seleccionar y escribir 2 letras se ejecuta el ajax
+                             * busca en la base de datos en el archivo autocompleteDiagnostico.php
+                             * el jSon correspondiente a las coincidencias
+                             * 
+                             * Funcion select que ejecutará una accion cuando se devuelva
+                             */        
+                          source: function( request, response ){
+                                $.ajax({
+                                    url: "../../../ajax/autocompleteAlergias.php",
+                                    data: {
+                                        name_startsWith: request.term
+                                    },
+                                    type: "post",
+                                    success: function( data ){
+                                        var output = jQuery.parseJSON(data);
+                                                                                
+                                        response( $.map( output, function( item ) {
+                                           return {
+                                               label: item.Nombre
+                                              ,id3 : item.idAlergias
+                                            }
+                                            
+                                        })//end map
+                                        );  // end response
+                                    }//end success
+
+                                }); // end ajax
+                            },  // end source
+                           select: function(event, ui){
+                                    $('#Alergias').removeAttr('idAlergia').attr('idAlergia',ui.item.id3)
+                                    $('#guardar_alergia').removeAttr('disabled');
+                                    $('#boton_alergia').removeAttr('disabled');
+                                },
+                           minLength: 2,
+                           open: function() {
+                                    $( this ).removeClass( "ui-corner-all" ).addClass( "ui-corner-top" );
+                                }, //end open
+                           close: function() {
+                                    $( this ).removeClass( "ui-corner-top" ).addClass( "ui-corner-all" );
+                                } //end close
+                            });//autocompleteDiagnosticos
 </script>
