d0e796e983d2bc22dd8f4cbb68775adec05420ac
Maximiano <maxsilva@alumnos.uai.cl>
2013-01-18 15:50:17 -0300

    nada

--- ajax/actualizarDatosPaciente.php
+++ ajax/actualizarDatosPaciente.php
@@ -1,10 +1,22 @@
 <?php
 
 include_once(dirname(__FILE__) . '/../Capa_Controladores/paciente.php');
+$idPaciente = 
 
 $actualizado = Paciente::ActualizarPorId($idPaciente);
 
 echo json_encode($actualizado);
+
+
+if ($actualizado != datos){
+    
+    return true;
+}
+else
+{
+    
+        return false;
+}
 
 
 ?>
