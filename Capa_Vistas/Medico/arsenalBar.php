2c5decf712c225fe441e93b4d80f4db801c6c0a7
mrbungie <geroa220@gmail.com>
2013-01-21 14:01:11 -0300

    Arsenal Bar Funcionando

--- Capa_Vistas/Medico/arsenalBar.php
+++ Capa_Vistas/Medico/arsenalBar.php
@@ -1,23 +1,34 @@
+
 <!-- barra de arsenal -->
 <div class="row-fluid show-grid img-rounded" id="arsenalBar" style="background-color: #7aba7b"> 
     <button class="btn btn-block" disabled>
         <i class="icon-list"></i> Arsenal
         <a href="#" class="closeBar"><i class="icon-remove pull-right"></i></a>
       </button>
-    <div id="medicamentosFav">
+    <div id="medicamentosArsenal">
        <div class="span10 offset1">
-           
-          <div class="alert alert-warning" identificador="1"><!-- pill medicamentoFav 1 -->
-              <strong>Medicamento Arsenal ej</strong>
-              <a href="#" rel="tooltip" title="Agregar a Favoritos"> <i class="icon-star pull-right"></i></a><!-- eliminar de favoritos --> 
-              <a href="#" rel="tooltip" title="Agregar a Receta" class="detalleMedicamento"> <i class="icon-plus pull-right"></i></a><!-- agregar favorito seleccionado -->
-          </div><!-- end pill diagnosticoFav 1-->
-          <div class="alert alert-warning" identificador="2"><!-- pill medicamentoFav 1 -->
-              <strong>Medicamento Arsenal ej</strong>
-              <a href="#" rel="tooltip" title="Agregar a Favoritos"> <i class="icon-star pull-right"></i></a><!-- eliminar de favoritos --> 
-              <a href="#" rel="tooltip" title="Agregar a Receta" class="detalleMedicamento"> <i class="icon-plus pull-right"></i></a><!-- agregar favorito seleccionado -->
-          </div><!-- end pill diagnosticoFav 1-->
-       
+          <?php
+		include_once(dirname(__FILE__).'/../../Capa_Datos/llamarQuery.php');
+		$sucursalRUT = $lugar['idSucursal'];
+		$queryString = "SELECT Nombre_Comercial, idMedicamento, Laboratorios.Nombre
+FROM Laboratorios, Arsenal, Medicamentos, Expendedores, Sucursales
+WHERE Medicamentos_idMedicamento = idMedicamento
+AND Expendedores_idExpendedores = idExpendedores
+AND RUT = Sucursales_RUT
+AND Laboratorio_idLaboratorio = Laboratorios.ID
+AND Sucursales_RUT =  '$sucursalRUT';";
+		$res = CallQuery($queryString);
+		while($row = $res->fetch_assoc()){
+			$nombre = $row['Nombre_Comercial'] . "-" . $row['Nombre'];
+			$id = $row['idMedicamento'];
+			echo "<div class='alert alert-warning' identificador='$id'>\r\n";
+			echo "<strong>$nombre</strong>\r\n";
+			echo "<a href='#' rel='tooltip' title='Agregar a Favoritos'> <i class='icon-star pull-right'></i></a><!-- eliminar de favoritos -->\r\n
+              			<a href='#' rel='tooltip' title='Agregar a Receta' class='detalleMedicamento'> <i class='icon-plus pull-right'></i></a><!-- agregar favorito seleccionado -->\r\n
+			</div>\r\n";
+		}
+          
+          ?>
        </div>
       </div>
     
