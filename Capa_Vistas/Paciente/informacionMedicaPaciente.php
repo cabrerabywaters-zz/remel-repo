741870bd018917788506a7ab8d0dffed978077c5
Leonardo Hidalgo <leonardo@gmail.com>
2013-01-21 14:01:24 -0300

    despliege de alegias en paciente correctamente

--- Capa_Vistas/Paciente/informacionMedicaPaciente.php
+++ Capa_Vistas/Paciente/informacionMedicaPaciente.php
@@ -4,84 +4,60 @@
     </a>
 </div>
 <div id="collapseone2" class="accordion-body collapse">
-    <div class="accordion-inner">
-
+    	<div class="accordion-inner">
         <div class="row">
-            <div class="span5" id="alergias">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Alergias</th>                      
-                        </tr>
-                    </thead>
-                    <tr><td>
-                            <table class="table table-hover">
-                                <?php
-                                if ($alergiasPaciente != false) {
-                                    foreach ($alergiasPaciente as $datos => $dato) {
-                                        echo"	<tr>";
-                                        $contador = 1;
-                                        foreach ($dato as $valor) {
-                                            $contador++;
-                                        }
+        <div class="span5" id="alergias">
 
-                                        echo"<td rowspan='" . $contador . "'>
-					</td></tr>";
-
-                                        foreach ($dato as $valor) {
-                                            $contador++;
-                                            echo "<tr><td>$valor</td></tr>";
-                                        }
-                                        echo"</tr>";
-                                    }
-                                }
-                                else{
-                                    echo 'el paciente no tiene alergias';
-                                }
-                                ?>
-                                </tbody>
-                            </table>
-                    </tr></td><tfoot><tr><td> 
-                            </td></tr></tfoot></table></div>
-
-
-
-            <div class="span5 offset2">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Condiciones</th>                      
-                        </tr>
-                    </thead>
-                    <tr><td>
-                            <table class="table table-hover">
-
-<?php
-if ($condicionesPaciente != false){
-foreach ($condicionesPaciente as $datos => $dato) {
-    echo"	<tr>";
-    $contador = 1;
-    foreach ($dato as $valor) {
-        $contador++;
-    }
-
-    echo"<td rowspan='" . $contador . "'>
-						</td></tr>";
-
-    foreach ($dato as $valor) {
-        $contador++;
-        echo "<tr><td>$valor</td></tr>";
-    }
-    echo"</tr>";
-}
-}
-else{
-    echo 'el paciente no tiene condiciones';
-}
+     <center> <div style="width: 50%; ;">
+  <table>
+   <thead>
+                     <tr>
+                     <th colspan="2">Alergias</th>                      
+                    </tr>
+                </thead>
+                <tr><td>
+   <table class="table table-hover">
+   <tbody>
+<tr><td><strong>Nombre</strong></td><td><strong>Sintomas</strong></td></tr>
+ <?php  foreach ($alergiasPaciente as $datos => $dato)
+   
+   {
+	echo "<tr>";
+	echo "<td>".$dato['Alergia']." </td>";
+	echo "<td>".$dato['Sintomas']."</td>";
+	
+	
+	echo "</tr>";   
+	   
+   }
+			   
+			   
+			   
+?> </tbody>
+            </table>
+            </td></tr></table></div></center></div>  
+		<div class="span5 offset2"><center><div style="width: 50%; ;">
+  <table>
+   <thead>
+                     <tr>
+                     <th>Condiciones</th>                      
+                    </tr>
+                </thead>
+                <tr><td>
+   <table class="table table-hover">
+   <tbody>
+  <?php  foreach ($condicionesPaciente as $datos => $dato)
+   {
+	echo "<tr>";
+	echo "<td>".$dato['Nombre']." </td>";
+	echo "</tr>";   
+	   
+   }
 ?>
-                                </tbody>
-                            </table>
-                    </tr></td><tfoot><tr><td> 
-                            </td></tr></tfoot></table></div></div>
+                </tbody>
+            </table>
+            </td></tr></table></div></center></div>
+
+		</div>
+        </div>
     </div>
-</div>
