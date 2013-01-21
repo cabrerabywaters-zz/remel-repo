9bd7e43484bef34ae7fc6c5f26dcb6703ce10a0c
José-Francisco González <pratiko2010@gmail.com>
2013-01-21 13:27:11 -0400

    arreglo de controladores de alergias y condiciones del paciente

--- Capa_Controladores/R_condicionesPaciente.php
+++ Capa_Controladores/R_condicionesPaciente.php
@@ -1,20 +1,41 @@
 <?php
 
-require_once '../Capa_Datos/R_condicionesPaciente.php';
+include_once(dirname(__FILE__) . '/../Capa_Datos/generadorStringQuery.php');
+include_once(dirname(__FILE__) . '/../Capa_Datos/interfazRelacion.php');
 
-function Creacion() {
-    $datosCreacion = array(
-        array('Paciente_idPaciente', $_POST['id_Paciente']),
-        array('Paciente_Personas_RUN', $_POST['run']),
-        array('Condiciones_idCondiciones', $_POST['id_Condicion'])
-    );
-    if ($_POST['id_Paciente'] != '' && $_POST['run'] != '' && $_POST['id_Condicion']) {
-        R_CondicionesPaciente::CrearRelacion($datosCreacion);
+class AlergiaHasPaciente {
+
+    static $nombreTabla = "Paciente_has_Condiciones";
+    static $nombreIdTabla = "Condiciones_idCondiciones";
+    static $nombreIdTabla1 = "Paciente_idPaciente";
+
+    public static function Insertar() {
+        $id1 = $_POST['Condiciones_idCondiciones'];
+        $id2 = $_POST['Paciente_idPaciente'];
+        $id = array($id1, $id2);
+        $queryString = QueryStringCrearRelacion($id, null, self::$nombreTabla);
+        $query = CallQuery($queryString);
     }
-    
-}
-function Eliminacion(){
-	$relacionABorrar = new R_CondicionesPaciente($_POST['id_Paciente'],$_POST['run'],$_POST['id_Condicion']);
-	$relacionABorrar->BorrarPorIdRelacion();
+
+    public static function BorrarPorId() {
+        $id1 = $_POST['Condiciones_idCondiciones'];
+        $id2 = $_POST['Paciente_idPaciente'];
+        $id = array($id1, $id2);
+
+        $nombreId = array(self::$nombreIdTabla, self::$nombreIdTabla1);
+
+        $queryString = QueryStringBorrarPorIdRelacion(self::$nombreTabla, $nombreId, $id);
+        $query = CallQuery($queryString);
+    }
+
+    public static function BuscarCondicionesPorPacienteId($idPaciente) {
+        $queryString = 'SELECT Condiciones_idCondiciones
+                        FROM Paciente_has_Condiciones
+                        WHERE Paciente_idPaciente = ' . $idPaciente . '
+                        ';
+        $resultado = CallQuery($queryString);
+        return $resultado;
+    }
+
 }
 ?>
