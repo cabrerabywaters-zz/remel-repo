75b4dbc0d0a39bbef4946f66c80c9de2d6dad5d7
César González Barreto <cgonzb@gmail.com>
2013-01-21 14:21:13 -0300

    modal de resumen receta

---
+++ Capa_Vistas/Medico/AtencionPaciente/resumenReceta.php
@@ -0,0 +1,16 @@
+ 
+<!-- Modal de resumen de la receta-->
+<div id="resumenReceta" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="resumenRecetaLabel" aria-hidden="true">
+  <div class="modal-header">
+    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+    <h3 id="resumenRecetaLabel">Resumen de la Receta <strong>Folio: #12345 </strong></h3>
+  </div>
+  <div class="modal-body" id="resumen">
+    <p>One fine body…</p>
+  </div>
+  <div class="modal-footer">
+    <button class="btn pull-left" data-dismiss="modal" aria-hidden="true"><br>Volver<br><br></button>
+    <button class="btn btn-primary"><br><strong><i class="icon-check icon-white"></i>Confirmar Emisión<br><br></strong></button>
+  </div>
+</div><!-- modal de resumen de la receta -->
+
