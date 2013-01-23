0727cd10a6118424bef124abe8b25b7eb6497017
César González Barreto <cgonzb@gmail.com>
2013-01-23 17:34:13 -0300

    modificacion visual del modal detalle medicamento

--- Capa_Vistas/Medico/AtencionPaciente/detalleMedicamento.php
+++ Capa_Vistas/Medico/AtencionPaciente/detalleMedicamento.php
@@ -25,29 +25,37 @@ include_once '../../../Capa_Controladores/unidadPeriodo.php';
         
         </div><!-- div de la imagen -->
         
-        <div class="row-fluid span11" id="detalleContenido">
-        <table>    
+        <div class="span10" id="detalleContenido">
+        <table class="table table-hover">    
             <tr>
                <td>Cantidad:</td>
-               <td><input type="text" placeholder="Indique Cantidad"  id="cantidadMedicamento"></td>
-               <td><select name="unidadDeConsumo"><?php foreach($unidadDeConsumo as $unidad){echo "<option value='".$unidad['idUnidad_de_Consumo']."'>".$unidad['tipo']."</option>";}?></select></td>
+               <td><input class="span10" type="text" placeholder="Indique Cantidad"  id="cantidadMedicamento" value="1"></td>
+               <td><select name="unidadDeConsumo" class="span11"><?php foreach($unidadDeConsumo as $unidad){echo "<option value='".$unidad['idUnidad_de_Consumo']."'>".$unidad['tipo']."</option>";}?></select></td>
             </tr>
             <tr>
                <td>Cada :</td>
-               <td><input type="text" placeholder="frequencia" id="frecuenciaMedicamento"></td>
-               <td><select name="unidadFrecuencia"><?php foreach($unidadFrecuencia as $unidad){echo "<option value='".$unidad['ID']."'>".$unidad['Nombre']."</option>";}?></select></td>
+               <td><input class="span10" type="text" placeholder="frequencia" id="frecuenciaMedicamento" value="8"></td>
+               <td><select name="unidadFrecuencia" class="span11"><?php foreach($unidadFrecuencia as $unidad){echo "<option value='".$unidad['ID']."'>".$unidad['Nombre']."</option>";}?></select></td>
             </tr>
             <tr>
                 <td>Por :</td>
-                <td><input type="text" placeholder="periodo" id="periodoMedicamento"></td>
-                <td><select name="unidadPeriodo"><?php foreach($unidadPeriodo as $unidad){echo "<option value='".$unidad['ID']."'>".$unidad['Nombre']."</option>";}?></select></td>
+                <td><input class="span10" type="text" placeholder="periodo" id="periodoMedicamento"></td>
+                <td><select name="unidadPeriodo" class="span11"><?php foreach($unidadPeriodo as $unidad){echo "<option value='".$unidad['ID']."'>".$unidad['Nombre']."</option>";}?></select></td>
             </tr>
-            <tr><td>Inicio</td><td><input type="text" name="fechaInicio" placeholder="Seleccionar inicio" class="datepicker"></tr>
-            <tr><td>Fin</td><td><input type="text" name="fechaFin" class="datepicker"></tr>
             <tr>
-                <td>Comentario:</td> 
+                <td>Inicio</td>
+                <td colspan="2"><input type="text" name="fechaInicio" placeholder="Seleccionar inicio" class="datepicker"></td>
             </tr>
-            <tr><td colspan="3"><textarea rows="2" style="width:95%" id="comentarioMedicamento"></textarea></td></tr>
+            <tr>
+                <td>Fin</td>
+                <td colspan="2"><input type="text" name="fechaFin" class="datepicker"></td>
+            </tr>
+            <tr>
+                <td colspan="3">Comentario:</td> 
+            </tr>
+            <tr>
+                <td colspan="3"><textarea width="100%" id="comentarioMedicamento"></textarea></td>
+            </tr>
         </table>
         </div>
         
@@ -57,6 +65,7 @@ include_once '../../../Capa_Controladores/unidadPeriodo.php';
     <div class="modal-footer"><!-- acciones del modal (cancerlar, agregar medicamento etc)-->
         <div class="pull-left">
             <select id="diagnosticoAsociado">
+                <option label="Seleccionar Diagnostico">Seleccionar Diagnostico</option>
                 <option value="0">Sin Diagnostico Asociado</option>
             </select>    
         </div>
@@ -68,13 +77,13 @@ include_once '../../../Capa_Controladores/unidadPeriodo.php';
 
 <script>
  function dayofyear(d) {   // d is a Date object
-var yn = d.getFullYear();
-var mn = d.getMonth();
-var dn = d.getDate();
-var d1 = new Date(yn,0,1,12,0,0); // noon on Jan. 1
-var d2 = new Date(yn,mn,dn,12,0,0); // noon on input date
-var ddiff = Math.round((d2-d1)/864e5);
-return ddiff+1; }   
+        var yn = d.getFullYear();
+        var mn = d.getMonth();
+        var dn = d.getDate();
+        var d1 = new Date(yn,0,1,12,0,0); // noon on Jan. 1
+        var d2 = new Date(yn,mn,dn,12,0,0); // noon on input date
+        var ddiff = Math.round((d2-d1)/864e5);
+        return ddiff+1; }   
 $('input[name="fechaInicio"]').change(function(){
    var inicio = $(this).val();
    var fecha = new Date(inicio);
