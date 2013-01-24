1cc238daadb35184da9fd0e405fc680ee8582194
Ignacio Cabrera Bywaters <cabrerabywaters@gmail.com>
2013-01-24 16:57:47 -0300

    Boton guardar para cambios de inforamcion del paciente

--- Capa_Controladores/region.php
+++ Capa_Controladores/region.php
@@ -48,7 +48,7 @@ class Region {
      */
     public static function Seleccionar($where, $limit = 0, $offset = 0) {
     	$atributosASeleccionar = array(
-                                        'IdRegion',
+                                        'idRegion',
                                         'Nombre',
                                         'Numero'
       );
