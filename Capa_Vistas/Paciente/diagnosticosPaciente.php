c9b8d67a74b0b4a37ac14c70604e0852c679547b
Maximiano <maxsilva@alumnos.uai.cl>
2013-01-24 18:01:06 -0300

    acutalizacoines

--- Capa_Vistas/Paciente/diagnosticosPaciente.php
+++ Capa_Vistas/Paciente/diagnosticosPaciente.php
@@ -1,29 +1,34 @@
 <div class="accordion-heading">
 <a class="btn btn-large btn-block active"  data-parent="#accordion2" >
-    Mis Diagnosticos
+    Mis Diagnósticos Históricos
 
 </a>
  </div>
   <div id="collapsetwo">
   <div class="accordion-inner">
-    <?php
-    echo'
+  
   <div class="row">
 
   <center> <div style="width: 50%; ;">
   <table>
                 <tr><td>
-   <table class="table table-striped">
+   <table class="table table-striped table-bordered table-condensed">
 	<thead>
     <tr>
-    <th>Diagnostico</td>
-    <th>Fecha</td>
-    <th>Nombre del Médico</td></tr></thead>
-    ';
+    <th>Diagnostico</th>
+    <th>Nombre del Médico</th>
+    <th>Fecha</th>
+    <th>Tipo</th>
+    <th>Comentario</th>
+    </tr>
+       </thead>
+       <tbody>
+    
+    <?php
     foreach ($diagnosticosPaciente as $datos => $dato) {
         echo "<tr>";
         foreach ($dato as $llave=>$valor) {
-            //var_dump($llave);
+           var_dump($llave);
             if ($llave == 'Nombre'){
                 echo '<td>';
                 echo $valor.' ';
@@ -42,7 +47,10 @@
             echo "</tr>";
     }
     echo '</table></div></table></center></div>';
-    ?>
+  ?>
+           
+           
+  </tbody>
   </div>
   </div>
 
