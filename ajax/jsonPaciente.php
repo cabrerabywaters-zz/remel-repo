9e43fba68d0331ba745a0125e8a6b6159905924d
José-Francisco González <pratiko2010@gmail.com>
2013-01-25 16:40:03 -0400

    verificacion de no usar el mismo rut para paciente

--- ajax/jsonPaciente.php
+++ ajax/jsonPaciente.php
@@ -1,15 +1,15 @@
 <?php
-
+session_start();
 include_once(dirname(__FILE__)."/../Capa_Controladores/persona.php");
 include_once(dirname(__FILE__)."/../Capa_Controladores/paciente.php");
 include_once(dirname(__FILE__)."/utilidades.php");
 
 $rut = validadorRUT($_POST['RUN']);
-
+if ($_SESSION['RUT'] != $_POST['RUN']){
 $array1 = Paciente::EncontrarPacienteAssoc($rut);
 $array2 = Persona::BuscarNombre($rut);
 
 $arrayFinal = array_merge($array1, $array2);
 echo json_encode($arrayFinal);
-
+} else echo 'error';
 ?>
