fed3627b38b170dee38b84ce192dd7a489211541
José-Francisco González <pratiko2010@gmail.com>
2013-01-29 11:18:33 -0400

    expendido completo, falta arreglar controladores

--- Capa_Vistas/Funcionario/expenderMedicamento.php
+++ Capa_Vistas/Funcionario/expenderMedicamento.php
@@ -4,41 +4,50 @@ iniciarCookie();
 verificarIP();
 include(dirname(__FILE__) . "/../funcionarioHeader.php");
 include(dirname(__FILE__) . "/../../Capa_Controladores/medicamento.php");
-
+print_r($_SESSION);
 //buscar datos del medicamento
 $datosMedicamento = Medicamento::BuscarMedicamentoPorId($_SESSION['medicamentoID']);
-print_r($datosMedicamento);
-echo '<br><br>';
-print_r($_SESSION);
-echo '<br>';
 echo $datosMedicamento['Nombre_Comercial'];
 echo '<br>';
-echo '<img src="' . $datosMedicamento['Foto_Presentacion'] . '" width="200" height="200"></img>';
+echo '<img src="'.$datosMedicamento['Foto_Presentacion'].'" width="200" height="200"></img>';
 echo '<br>';
-echo '<input type="text" id="cantidad" value="1">';
+echo '<input type="text" id="numero" value="1">';
 echo '<br>';
-echo '<button class="btn btn-primary" onClick="expender()" type="submit"><strong>Expender</strong></button>';
+echo '<input class="btn btn-primary" id="expender" type="submit"><strong>Expender</strong></input>';
 ?>
-
+<html>
 <script>
+    //$(document).ready(function(){
     
-    function expender(){
-        var cantidad = document.getElementById('cantidad').value;
+    $('#expender').click(function(){
+        var cantidad = document.getElementById('numero').value;
+
+        $.ajax({ url: '../../ajax/expenderMedicamento.php',
+            data: {'cantidad': cantidad},
+            type: 'post',
+            success: function(output) {
+                alert(output);
+                if(output == 1){// redireccion a atencionPaciente
+                    window.location.href = 'medicamentoExpendido.php#';
+                }                
+            }
+        });
+    })
+    
+    /*function expender(){
+        var cantidad = document.getElementById("cantidad").value;
         $.ajax({ url: '../../ajax/expenderMedicamento.php',
             data: {'cantidad': cantidad },
             type: 'post',
             success: function(output) {
                 if(output == 1){// redireccion a atencionPaciente
                     window.location.href = 'medicamentoExpendido.php#';
-                    
                 }                
                 else{    
-                                    
-                    $("#mensaje").html("<div class='alert alert-error'>La Clave no es correcta</div>");
                 }
             }
         });
-
     }
-    
+    //});*/
 </script>
+</html>
--- Capa_Vistas/Funcionario/medicamentoExpendido.php
+++ Capa_Vistas/Funcionario/medicamentoExpendido.php
@@ -1,7 +1,19 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+include_once(dirname(__FILE__) . '/../../ajax/sessionCheck.php');
+iniciarCookie();
+verificarIP();
+include(dirname(__FILE__) . "/../funcionarioHeader.php");
+include(dirname(__FILE__) . "/../../Capa_Controladores/medicamentoVendido.php");
+
+$fechaActual = date("Y-m-d H:i:s");
+$med = MedicamentoVendido::InsertarConParametros($_SESSION['logLugar']['idLugar'], 
+                                                                $_SESSION['medicamentoID'],
+                                                                $_SESSION['recetaID'], 
+                                                                $_SESSION['cantidadMedicamentoVendido'], 
+                                                                $fechaActual, 
+                                                                $_SESSION['unidadID']);
+if ($med){
+ echo 'Medicamento Expendido Exitosamente.';   
+}
 ?>
