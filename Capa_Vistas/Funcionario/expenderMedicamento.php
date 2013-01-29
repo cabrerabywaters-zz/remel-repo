f7079fd61187f30dac499abdeb12cda6ab756014
José-Francisco González <pratiko2010@gmail.com>
2013-01-29 15:04:46 -0400

    fix de boton submit

--- Capa_Vistas/Funcionario/expenderMedicamento.php
+++ Capa_Vistas/Funcionario/expenderMedicamento.php
@@ -13,11 +13,10 @@ echo '<img src="'.$datosMedicamento['Foto_Presentacion'].'" width="200" height="
 echo '<br>';
 echo '<input type="text" id="numero" value="1">';
 echo '<br>';
-echo '<input class="btn btn-primary" id="expender" type="submit"><strong>Expender</strong></input>';
+echo '<input class="btn btn-primary" id="expender" type="submit" value="Expender"></input>';
 ?>
 <html>
 <script>
-    //$(document).ready(function(){
     
     $('#expender').click(function(){
         var cantidad = document.getElementById('numero').value;
@@ -34,20 +33,5 @@ echo '<input class="btn btn-primary" id="expender" type="submit"><strong>Expende
         });
     })
     
-    /*function expender(){
-        var cantidad = document.getElementById("cantidad").value;
-        $.ajax({ url: '../../ajax/expenderMedicamento.php',
-            data: {'cantidad': cantidad },
-            type: 'post',
-            success: function(output) {
-                if(output == 1){// redireccion a atencionPaciente
-                    window.location.href = 'medicamentoExpendido.php#';
-                }                
-                else{    
-                }
-            }
-        });
-    }
-    //});*/
 </script>
 </html>
