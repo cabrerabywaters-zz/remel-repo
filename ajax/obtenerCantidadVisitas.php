18d9ced6b99b3c047ca7d6823de960857db8c615
mrbungie <geroa220@gmail.com>
2013-02-06 16:45:22 -0300

    nada que hacer

--- ajax/obtenerCantidadVisitas.php
+++ ajax/obtenerCantidadVisitas.php
@@ -3,7 +3,8 @@
 include_once(dirname(__FILE__)."/../Capa_Datos/llamarQuery.php");
 
 $idPaciente = $_SESSION['idPaciente'];
-$queryString = "SELECT CONCAT(Nombre, ' ',Apellido_Paterno) as Nombre, Count(Fecha) FROM Consulta, Medicos, Personas WHERE Pacientes_idPaciente = '$idPaciente' AND Medicos_idMedico = Medicos.idMedico AND Medicos.Personas_RUN = Personas.RUN GROUP BY Medicos_idMedico LIMIT 5;";
+$queryString = "SELECT CONCAT(Nombre, ' ',Apellido_Paterno) as Nombre, Count(idReceta) FROM Consulta, Medicos, Personas, Recetas WHERE Pacientes_idPaciente = '$idPaciente' AND Medicos_idMedico = Medicos.idMedico AND Medicos.Personas_RUN = Personas.RUN AND Recetas.Consulta_Id_Consulta = Consulta.Id_Consulta GROUP BY Medicos_idMedico LIMIT 5;";
+echo $queryString;
 $query = CallQuery($queryString);
 $doctores = array();
 while($row = $query->fetch_array(MYSQLI_NUM)){
